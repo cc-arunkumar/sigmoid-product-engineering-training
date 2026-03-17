ae7a09b2a3a8336b8fa94001b56f6835d7b2eafb
AS-MAC-1183 <as-mac-1183@AS-MAC-1183.local>
2026-03-17 15:19:45 +0530

    feat(middleware): add application-level logging middleware

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
-
+import { logger } from "./middleware/logger.js";
 const app = express()
 
 import productRoutes from "./routes/productRoutes.js"
 app.use(express.json());
 app.use(productRoutes);
+app.use(logger);
 app.listen(3000, () => {
     console.log("SERVER ON 3000")
 })
---
+++ templates/week1/day1-node-rest-api/src/middleware/logger.js
@@ -0,0 +1,8 @@
+export const logger = (req,res,next)=>{
+const method = req.method;
+const url = req.url;
+const time = new Date().toISOString();
+
+console.log(`${method},${url} - ${time}`);
+next();
+}

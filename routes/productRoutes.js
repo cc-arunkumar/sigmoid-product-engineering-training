ab9b36c43d6707337fdabd1c344459ec7029922d
AS-MAC-1225 <as-mac-1225@AS-MAC-1225.local>
2026-03-17 16:10:04 +0530

    feat(middleware): add product validation middleware for POST, PUT and PATCH routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,18 @@
 const express=require("express");
 const router=express.Router();
 const productController=require("../controllers/productController");
+const validateProduct=require("../middleware/validateProduct");
+const validateProductPatch = require("../middleware/validateProductPatch")
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id",productController.getProductById);
 router.post("/api/products",productController.createProduct);
 router.put("/api/products/:id",productController.updateproduct);
 router.delete("/api/products/:id",productController.deleteproduct);
 router.patch("/api/products/:id",productController.updatePartialProduct);
+
+router.post("/api/products",validateProduct,productController.createProduct);
+router.put("/api/products/:id",validateProduct,productController.updateproduct);
+router.patch("/api/products/:id",validateProductPatch,productController.updatePartialProduct);
+
+
 module.exports=router;

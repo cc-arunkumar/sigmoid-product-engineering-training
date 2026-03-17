864809c1057c9d3773ac611933f5ffa7198698d7
Sai Ganesh <as-mac-1228@AS-MAC-1228.local>
2026-03-17 13:33:18 +0530

    feat(api): implement DELETE /users/:id

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,3 +34,17 @@ exports.updateProduct=(req,res)=>{
     product.price=price;
     return res.status(200).json(product);
 }
+
+exports.deleteProduct=(req,res)=>{
+    const productId=parseInt(req.params.id);
+    const productIndex=products.findIndex(p=>p.id===productId);
+    if (productIndex===-1){
+        return res.status(404).json({
+            message:"Product not found"
+        });
+    }
+    products.splice(productIndex,1);
+    return res.status(200).json({
+        message:"Product deleted"
+    });
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,5 +8,6 @@ router.get("/api/products",productController.getAllProducts);
 router.get("/api/products/:id",productController.getProductById);
 router.post("/api/productspost",productController.postProduct);
 router.put("/api/products",productController.updateProduct);
+router.delete("/api/products/:id",productController.deleteProduct);
 
 module.exports=router;

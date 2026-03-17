312a72a3d9409da8ec14116fb25e7a34107600d0
AS-MAC-1225 <as-mac-1225@AS-MAC-1225.local>
2026-03-17 14:54:37 +0530

    feat(api): get product by id implemented

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,3 +2,14 @@ const products=require("../data/products");
 exports.getAllProducts=(req,res)=>{
     res.json(products);
 };
+exports.getProductById=(req, res)=>{
+    const productId=parseInt(req.params.id);
+    const product=products.find(p=> p.id===productId);
+
+    if(!product){
+        return res.status(400).json({
+            message:"Product not found"
+        });
+    }
+    res.json(product);
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,4 +2,5 @@ const express=require("express");
 const router=express.Router();
 const productController=require("../controllers/productController");
 router.get("/api/products", productController.getAllProducts);
+router.get("/api/products/:id",productController.getProductById);
 module.exports=router;

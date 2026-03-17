761fe2dd84110457605f494a217c07d77a3e4fdd
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-17 12:56:05 +0530

    feat(api): implement GET /users

--- templates/week1/day1-node-rest-api/src/routes/userRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/userRoutes.js
@@ -9,18 +9,7 @@ const { routes } = require("../app");
 // Routes
 
 routes.get("/users", getAllUsers);
-router.get("/users/:id", (req, res) => {
-  res.send("Get User by ID");
-});
-router.post("/users", (req, res) => {
-  res.send("Create User");
-});
-router.put("/users/:id", (req, res) => {
-  res.send("Update User");
-});
-router.delete("/users/:id", (req, res) => {
-  res.send("Delete User");
-});
+
 
 
 module.exports = router;

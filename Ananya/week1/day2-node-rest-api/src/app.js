b64d67923db77c5950dc07f17dba2e9ae7e3cf9d
AS-MAC-1197 <as-mac-1197@AS-MAC-1197s-MacBook-Air.local>
2026-03-17 15:28:40 +0530

    feat(middleware): add application-level logging middleware

--- Ananya/week1/day2-node-rest-api/src/app.js
+++ Ananya/week1/day2-node-rest-api/src/app.js
@@ -2,9 +2,10 @@ const express= require("express") // express is module with which we will create
 const app = express() 
 
 const productRoutes= require("./routes/productRouter");
+const loggers= require("./middleware/logger");
  // middleware so that our express understands the data send in the json format
  app.use(express.json());
-
+app.use(loggers);
 app.use(productRoutes);
 
 app.listen(3000, ()=>{

bdf07a293db6ede8481149c4342b8fd15fe7674e
yuvika-sigmoid <yuvika.pothrapally@sigmoidanalytics.com>
2026-03-17 12:56:44 +0530

    feat(api): implement GET /users

--- templates/week1/day1-node-rest-api/src/routes/usersRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/usersRoutes.js
@@ -8,11 +8,6 @@ const { route } = require("../app");
 
 // Routes
 router.get("/", getAllUsers);
-router.post("/", (req, res) => {
-  res.send("Create a new user");
-});
-router.get("/:id", (req, res) => {
-  res.send(`Get user with ID ${req.params.id}`);
-});
+
 
 module.exports = router;

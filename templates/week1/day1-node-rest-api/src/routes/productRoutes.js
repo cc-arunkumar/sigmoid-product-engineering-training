5e2faf37c8e31a96fc16ca6973bff0428ce6e614
AS-MAC-1239 <as-mac-1239@AS-MAC-1239.local>
2026-03-17 12:47:59 +0530

    Get Products By Id Created

--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -3,9 +3,6 @@ const router=express.Router();
 const productController=require("../controllers/productController");
 router.get("/products",productController.getAllProducts);
 router.get("/products/:id",productController.getProductsById)
-router.post("/products",productController.createProduct);
-router.put("/products/:id",productController.updateProduct)
-router.delete("/products/:id",productController.deleteProduct)
-router.patch("/products/:id",productController.updatePartialProduct)
+
 
 module.exports=router;

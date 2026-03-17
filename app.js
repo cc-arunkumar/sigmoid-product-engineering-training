1455e326dc1c1e11c3a08bbf72f535d16075eb39
AS-MAC-1116 <as-mac-1116@AS-MAC-1116.local>
2026-03-17 17:44:36 +0530

    feat(middleware): add centralized error handling middleware

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ app.get("/",(req,res)=>{
 });
 
 const productRoutes=require("./routes/productRoutes")
-const logger=require("./middleware/logger")
+const logger=require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());  // Middleware to parse JSON request bodies 
 
 app.use(productRoutes);  // Use the product routes for handling requests to /products and /product/:id
 
 app.use(logger);  // Use the logger middleware to log incoming requests
+
+app.use(errorHandler);  // Use the error handling middleware to catch and handle errors in the application
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

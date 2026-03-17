0802f071bf85b1e31f11f0cce6310352fb435555
AS-MAC-1116 <as-mac-1116@AS-MAC-1116.local>
2026-03-17 13:19:38 +0530

    feat(api): implement GET /products/:id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,6 @@ const router=express.Router();
 const productController=require("../controllers/productController")
 
 router.get("/products",productController.getProducts)
-
+router.get("/product/:id",productController.getProductById)
 
 module.exports=router;

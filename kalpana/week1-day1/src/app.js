38a6845b15f2b807f6669567af5f710716a797be
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 15:46:56 +0530

    access logger in app.js

--- kalpana/week1-day1/src/app.js
+++ kalpana/week1-day1/src/app.js
@@ -5,6 +5,9 @@ const productRoutes = require("./routes/productRoutes");
 app.use(express.json());
 app.use(productRoutes);
 
+const logger = require("./middleware/logger");
+app.use(logger);
+
 app.listen(3000, ()=>{
     console.log("server is running....")
 })

d800fd49cf36b6750f69b591fff537d542ded2f2
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-17 13:01:08 +0530

    feat: add product routes with GET /products endpoint

--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -1,19 +1,4 @@
-const express = require("express");
-const router = express.Router();
-
-const {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
-} = require("../controllers/userController");
-
-// Routes
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-router.post("/", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-
-module.exports = router;
+const express=require("express")
+const router=express.Router();
+const productController=require("../controllers/productController");
+router.get("/products",productController.getAllProducts);

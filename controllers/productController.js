815f0c14ec6ca8c856746127cb8d10f21e83ba7b
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-03-17 13:35:26 +0530

    feat: add GET /product:id to get product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,3 +3,16 @@ const products = require("../data/products");
 exports.getAllProducts = (req, res) => {
     res.json(products);
 }
+
+exports.getProductById = (req, res) => {
+    const productId = parseInt(req.params.id);
+    const product = products.find(p => p.id === productId);
+
+    if(!product){
+        return res.status(400).json({
+            message: "Product Not found"
+        });
+    };
+
+    res.json(product);
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const productController = require("../controllers/productController")
 
 router.get("/api/products", productController.getAllProducts);
+router.get("/api/product/:id", productController.getProductById);
+
 
 module.exports = router;

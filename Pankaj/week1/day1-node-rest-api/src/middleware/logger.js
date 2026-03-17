dd6036e347b706867a462b362da7c18c103c0949
AS-MAC-1339 <as-mac-1339@AS-MAC-1339s-MacBook-Air.local>
2026-03-17 15:22:40 +0530

    feat(middleware): add application-level logging middleware

--- Pankaj/week1/day1-node-rest-api/src/middleware/logger.js
+++ Pankaj/week1/day1-node-rest-api/src/middleware/logger.js
@@ -2,7 +2,7 @@ const logger = (req, res, next) => {
     const method = req.method;
     const url = req.url;
     const timestamp = new Date().toISOString();
-    console.log(` ${method} ${url} - ${timestamp}`);
+    console.log(` ${method} ${url} - ${timestamp}`) ;
     next();
 };
 

71d8e4e337ef0f6688d5266571dd528ab25b6c63
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-17 13:03:02 +0530

    feat: add getProductById API with 404 error handling

--- templates/week1/day1-node-rest-api/src/controllers/productController.js
+++ templates/week1/day1-node-rest-api/src/controllers/productController.js
@@ -2,3 +2,15 @@ const products=require("../data/products")
 exports.getAllProducts=(req,res)=>{
     res.json(products);
 }
+exports.getProductById=(req,res)=>{
+    const productId=parseInt(req.params.id);
+    const product=products.find(p=>p.id===productId);
+    if(!product)
+    {
+        return res.status(404).json({
+           
+            error:`not found this ${productId} in product list`
+        });
+    }
+    res.json(product);
+};

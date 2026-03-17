c1cb7e1150fd74c99bf8d7857d6394ca4cd9cabf
AS-MAC-1225 <as-mac-1225@AS-MAC-1225.local>
2026-03-17 14:55:50 +0530

    feat(api): implement POST /users endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,3 +13,16 @@ exports.getProductById=(req, res)=>{
     }
     res.json(product);
 };
+exports.createProduct=(req,res)=>{
+    const{name, price,category,stock}=req.body
+
+    const product={
+        id: products.length +101,
+        name:name,
+        price:price,
+        category:category,
+        stock:stock
+    }
+    products.push(product)
+    res.status(201).json(product)
+};

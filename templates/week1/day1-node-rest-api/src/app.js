81e15576712a209d24fc0806f8a4b9617368e8c6
AS-MAC-1161 <as-mac-@AS-MAC-1161s-MacBook-Air.local>
2026-03-17 12:41:49 +0530

    Initialize server setup

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const userRoutes = require("./routes/userRoutes");
+const express = require("express") //express module
 
-const app = express();
+const app = express()//initialization of library , app const is storing the express library
 
-app.use(express.json());
-
-// Register routes
-app.use("/api/users", userRoutes);
-
-module.exports = app;
+app.get("/",(req,res)=>{
+    res.send("Welcome backend"); //pass req,res (http objects) , return be the message to the UI in body
+}) //default API we are trying to hit , path to api endpoint
+app.listen(3000,()=>{ //from client to connect to the server use port
+    console.log("server started"); //launch the server using node index.js hit enter calls comes here
+})

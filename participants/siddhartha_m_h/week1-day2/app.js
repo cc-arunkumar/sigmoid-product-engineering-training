26a7e5706baf259022a395b706a890e8a14136b4
AS-MSC-1310 <as-msc-1310@AS-MAC-1310.local>
2026-03-17 17:32:28 +0530

    feat(middleware): add centralized error handling middleware

--- participants/siddhartha_m_h/week1-day2/app.js
+++ participants/siddhartha_m_h/week1-day2/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const productRoutes = require('./routes/product');
 const logger = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 
 const app = express();
 
 app.use(express.json());
 app.use(logger);
+app.use(errorHandler);
 
 
 app.use('/api/products', productRoutes);

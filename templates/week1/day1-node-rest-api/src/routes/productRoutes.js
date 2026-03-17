f78bd8804be8abd2e361f7e700647d9285747f5e
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-17 13:54:51 +0530

    feat(products) add products data

---
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const productController=require("../controllers/productControllers");
+router.get("/products",productController.getAllProducts)
+module.exports = router;

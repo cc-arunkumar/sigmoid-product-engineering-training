325d96e74cfb8ecb97a20b60ef5cf15c8916dc61
AS-MAC-1239 <as-mac-1239@AS-MAC-1239.local>
2026-03-17 12:59:19 +0530

    Get Products By Id

--- templates/week1/day1-node-rest-api/src/controllers/productControllers.js
+++ templates/week1/day1-node-rest-api/src/controllers/productControllers.js
@@ -2,7 +2,18 @@ const products=require("../data/products.js");
 exports.getAllProducts=(req,res)=>{
     res.json(products);
 }
-
+exports.getProductsById=(req,res)=>{
+   
+    const productId=parseInt(req.params.id);
+    console.log(productId)
+    const product=products.find(p=>p.id===productId);
+    if(!product){
+        return res.status(400).json({
+            message:"Product not found"
+        });
+    }
+    res.json(product);
+};
 
 
 
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -2,6 +2,6 @@ const express=require("express");
 const router=express.Router();
 const productController=require("../controllers/productController");
 router.get("/products",productController.getAllProducts);
-
+router.get("/products/:id",productController.getProductsById)
 
 module.exports=router;

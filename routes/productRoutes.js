598d00a388d0644f82962d5dd08d29685d25b381
cs-ganesh <chanda.sai@sigmoidanalytics.com>
2026-03-17 17:15:57 +0530

    feat(middleware): add product validation middleware for POST and PUT routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,12 +4,13 @@ const router=express.Router();
 
 const productController=require("../controllers/productController.js");
 const validateProduct = require("../middleware/validateProduct.js");
+const validateProductPartial=require("../middleware/validateProductPartial.js");
 
 router.get("/api/products",productController.getAllProducts);
 router.get("/api/products/:id",productController.getProductById);
 router.post("/api/productspost", validateProduct ,productController.postProduct);
 router.put("/api/products", validateProduct,productController.updateProduct); 
 router.delete("/api/products/:id",productController.deleteProduct);
-router.patch('/api/products/:id', productController.updatePartialProduct);
+router.patch('/api/products/:id', validateProductPartial ,productController.updatePartialProduct);
 
 module.exports=router;

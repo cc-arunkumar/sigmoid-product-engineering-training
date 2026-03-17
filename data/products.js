703eab52827bef122c25d0435ffe2b7129bee039
Shubham <220108059shubham@gmail.com>
2026-03-17 13:00:54 +0530

    added product.jsin data

---
+++ data/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    "id": 21,
+    "name": "Wireless Mouse",
+    "price": 599,
+    "category": "Electronics",
+    "stock": 50,
+    "brand": "Logitech"
+  },
+  {
+    "id": 2,
+    "name": "Bluetooth Headphones",
+    "price": 1499,
+    "category": "Electronics",
+    "stock": 30,
+    "brand": "Boat"
+  },
+  {
+    "id": 3,
+    "name": "Laptop Stand",
+    "price": 899,
+    "category": "Accessories",
+    "stock": 20,
+    "brand": "Portronics"
+  },
+  {
+    "id": 4,
+    "name": "Mechanical Keyboard",
+    "price": 3499,
+    "category": "Electronics",
+    "stock": 15,
+    "brand": "Redragon"
+  }
+]
+
+module.exports = products;

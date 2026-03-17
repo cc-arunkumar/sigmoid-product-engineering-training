e831a4ec47ece5da38507a237e7f92fa3de1031c
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-17 13:05:18 +0530

    Add createProduct API to add new product

--- templates/week1/day1-node-rest-api/src/controllers/productController.js
+++ templates/week1/day1-node-rest-api/src/controllers/productController.js
@@ -14,3 +14,17 @@ exports.getProductById=(req,res)=>{
     }
     res.json(product);
 };
+exports.createProduct=(req,res)=>{
+    const {name,price,category,stock}=req.body;
+    const newproduct={
+        id:products.length+101,
+        name:name,
+        price:price,
+        category:category,
+        stock:stock
+    }
+   products.push(newproduct)
+   res.status(201).json({
+    message:"product added successfully"
+   })
+}

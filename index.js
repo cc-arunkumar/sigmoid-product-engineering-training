886ce6df405b7a45c88dc6dafd869dfe024f3051
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-03-17 17:34:31 +0530

    feat(middleware): add centralized error handling middleware

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 
 const productRoutes = require("./routes/productRoutes")
 const logger = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
 
 app.use(express.json())
 app.use(logger)
+app.use(errorHandler)
 app.use(productRoutes)
 
 app.listen(3000, () => {

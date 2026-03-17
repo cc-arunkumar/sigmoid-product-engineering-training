7428a807d2b30b1fca4dde3f32302d927707e94f
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-03-17 13:43:10 +0530

    feat: add POST /products to create a product

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,3 +16,18 @@ exports.getProductById = (req, res) => {
 
     res.json(product);
 };
+
+exports.createProduct = (req, res) => {
+    const {name, price, category, stock} = req.body
+
+    const product = {
+        id : products.length + 101,
+        name: name, 
+        price: price,
+        category: category,
+        stock: stock
+    }
+
+    products.push(product)
+    res.status(201).json(product)
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ const productController = require("../controllers/productController")
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/product/:id", productController.getProductById);
+router.post("/api/products", productController.createProduct);
 
 
 module.exports = router;

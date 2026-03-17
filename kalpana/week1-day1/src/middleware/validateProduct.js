149a612479d5d20731cc2df43a666d22baf89244
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 16:03:23 +0530

    feat(middleware): add product validation middleware for POST, PUT and PATCH routes

---
+++ kalpana/week1-day1/src/middleware/validateProduct.js
@@ -0,0 +1,32 @@
+const validateProduct = (req, res, next) => {
+    const { name, price, category, stock } = req.body;
+
+    if (!name || name.trim() === "" || name.length() <= 0) {
+        return res.status(400).json({
+            success: false,
+            message: "product name is required"
+        });
+    }
+
+    if (price <= 0 || price === undefined) {
+        return res.status(400).json({
+            success: false,
+            message: "price must be greater than 0"
+        })
+    }
+    if (!category || category.trim() === "") {
+        return res.status(400).json({
+            success: false,
+            message: "Category is required"
+        });
+    }
+    if (stock === undefined || stock < 0) {
+        return res.status(400).json({
+                success: false,
+                message: "Stock cannot be negative"
+        });
+    }
+    next();
+}
+
+module.exports = validateProduct ;
--- kalpana/week1-day1/src/routes/productRoutes.js
+++ kalpana/week1-day1/src/routes/productRoutes.js
@@ -2,16 +2,38 @@ const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get('/api/products', productController.getAllProducts);
+const validateProduct = require("../middleware/validateProduct");
 
-router.get("/api/product/:id", productController.getProductById);
+router.get("/api/products", productController.getAllProducts);
+router.get("/api/products/:id", productController.getProductById);
+router.post(
+    "/api/products",
+    validateProduct,
+    productController.createProduct
+);
+router.put(
+    "/api/products/:id",
+    validateProduct,
+    productController.updateProduct
+);
+router.patch(
+    "/api/products/:id",
+    validateProduct,
+    productController.updatePartialProduct
+);
+module.exports = router;
 
-router.post("/api/products", productController.createProduct);
+// router.get('/api/products', productController.getAllProducts);
 
-router.put("/api/product/:id", productController.updateProduct);
+// router.get("/api/product/:id", productController.getProductById);
+
+// router.post("/api/products", productController.createProduct);
+
+// router.put("/api/product/:id", productController.updateProduct);
 
 router.delete("/api/product/:id", productController.deleteProductById);
 
-router.patch("/api/product/:id", productController.updatePartialProduct);
+// router.patch("/api/product/:id", productController.updatePartialProduct);
 
 module.exports = router;
+

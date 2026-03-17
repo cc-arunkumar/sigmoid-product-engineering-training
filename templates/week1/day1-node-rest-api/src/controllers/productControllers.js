af2b442d2d90ac604be97dff7ead7e71999b4808
AS-MAC-1239 <as-mac-1239@AS-MAC-1239.local>
2026-03-17 12:55:33 +0530

    Get All Products

--- templates/week1/day1-node-rest-api/src/controllers/productControllers.js
+++ templates/week1/day1-node-rest-api/src/controllers/productControllers.js
@@ -2,32 +2,8 @@ const products=require("../data/products.js");
 exports.getAllProducts=(req,res)=>{
     res.json(products);
 }
-exports.getProductsById=(req,res)=>{
-   
-    const productId=parseInt(req.params.id);
-    console.log(productId)
-    const product=products.find(p=>p.id===productId);
-    if(!product){
-        return res.status(400).json({
-            message:"Product not found"
-        });
-    }
-    res.json(product);
-};
-exports.createProduct=(req,res)=>{
-    const {name,price,category,stock}=req.body
-    const product={
-        id:products.length+101,
-        name:name,
-        price:price,
-        category:category,
-        stock:stock
 
 
-    }
-    products.push(product)
-    res.status(201).json(product)
-}
 
 
 
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -2,7 +2,6 @@ const express=require("express");
 const router=express.Router();
 const productController=require("../controllers/productController");
 router.get("/products",productController.getAllProducts);
-router.get("/products/:id",productController.getProductsById)
-router.post("/products",productController.createProduct);
+
 
 module.exports=router;

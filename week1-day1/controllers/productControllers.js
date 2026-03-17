6337ddd8ea5214188dc3597f1b45cac3d7987733
Aditya <aditya.kumar26@sigmoidanalytics.com>
2026-03-17 17:16:09 +0530

    feat(api): implement GET /product/:id

--- week1-day1/controllers/productControllers.js
+++ week1-day1/controllers/productControllers.js
@@ -3,3 +3,12 @@ const products = require("../data/products")
 exports.getAll = (req, res) => {
     res.json(products);
 }
+
+exports.getProduct = (req, res) => {
+    const productId = Number(req.params.id);
+    const product = products.find(p => p.id === productId);
+
+    if(!product) res.status(404).json({message: `No product found with product id - ${productId}.`});
+
+    res.json(product);
+}
--- week1-day1/routes/productRoutes.js
+++ week1-day1/routes/productRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getAll } = require("../controllers/productControllers");
+const { getAll, getProduct } = require("../controllers/productControllers");
 const router = express.Router();
 
 router.get("/products", getAll);
+router.get("/product/:id", getProduct);
 
 module.exports = router;

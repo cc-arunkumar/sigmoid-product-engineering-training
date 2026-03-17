5a475c7b758a327959969630e26c1ee67119fecb
AS-MAC-1183 <as-mac-1183@AS-MAC-1183.local>
2026-03-17 12:49:48 +0530

    feat(server): configure Express server

---
+++ templates/week1/day1-node-rest-api/src/data/products.js
@@ -0,0 +1,21 @@
+export const products = [
+        {
+            id:1,
+            name:"phone",
+            price:37000.00,
+            stock:20
+        }
+        , 
+        {
+            id : 2 , 
+            name : "earpods",
+            price:13000
+        },
+        {
+            id:3 ,
+            name : "keyboard",
+            price : 15000
+        }
+    ]
+
+export default products;

cfa243915fa390378e31756c7d479f25d9152f12
Utkarsh Kumar <utkarsh.kumar@sigmoidanalytics.com>
2026-03-17 16:11:41 +0530

    solved the bug in data

--- data/product_data.js
+++ data/product_data.js
@@ -1,5 +1,5 @@
 export  const products=[
-    {id:1, name:"Nike Shoe", price: 120},
-    {id:2, name:"Adidas Shirt", price: 100},
-    {id:3, name:"Puma Pant", price: 90}
+    {id:1, name:"Nike Shoe", price: 120, category:"Footwear", stock: 50},
+    {id:2, name:"Adidas Shirt", price: 100, category:"Apparel", stock: 100},
+    {id:3, name:"Puma Pant", price: 90, category:"Apparel", stock: 75}
 ]

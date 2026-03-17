4818fa924223e220aed44fd344c4545075a4d3f8
AS-MAC-1183 <as-mac-1183@AS-MAC-1183.local>
2026-03-17 13:01:32 +0530

    feat(api): implement GET /users/:id

--- templates/week1/day1-node-rest-api/src/controllers/productControllers.js
+++ templates/week1/day1-node-rest-api/src/controllers/productControllers.js
@@ -5,3 +5,13 @@ export const getAllProducts =  (req,res) => {
     res.json(products);
 };
 
+export const getProductById = (req,res) => {
+    const productId = (req.params.id);
+    const product = products.find(p=> p.id == productId);
+    if(!product){
+        return res.status(404).json({message:"Product not found"});
+    }
+    res.json(product);
+
+};
+
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getAllProducts} from "../controllers/productControllers.js";
+import { getAllProducts,getProductById} from "../controllers/productControllers.js";
 const router = express.Router();
 router.get("/products" , getAllProducts);
+router.get("/product/:id" ,getProductById);
 
 export default router;

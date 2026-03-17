c1bddfed194d9b2a892c43cdc345eafde0247977
Anish <anish01022@gmail.com>
2026-03-17 13:31:40 +0530

    Standardize API routes with /api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express() 
 
 const productRoutes = require("./routes/productRoutes");
+
 app.use(express.json());
+
 app.use(productRoutes);
 app.listen(3000 , () => {
     console.log("server is being started")
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router();
 const productController = require("../controllers/productController");
-router.get("/products" , productController.getALLProducts);
-router.get("/products/:id", productController.getProductById);
-router.post("/products/create" , productController.createProduct);
-router.put("/products/:id", productController.updateProduct);
-router.delete('/products/delete/:id', productController.deleteProduct)
-router.patch('/products/:id' , productController.patchProduct)
+router.get("/api/products" , productController.getALLProducts);
+router.get("/api/products/:id", productController.getProductById);
+router.post("/api/products/create" , productController.createProduct);
+router.put("/api/products/:id", productController.updateProduct);
+router.delete('/api/products/delete/:id', productController.deleteProduct)
+router.patch('/api/products/:id' , productController.patchProduct)
 module.exports = router;

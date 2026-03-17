4eda2bea9789490afbf720ba924eb4dd9c2acc02
Brxj19 <kumarbraj1925@gmail.com>
2026-03-17 12:27:57 +0530

    chore(project): setup initial project structure and server configuration

--- templates/week1/day1-node-rest-api/src/server.js
+++ templates/week1/day1-node-rest-api/src/server.js
@@ -1,7 +1,10 @@
-const app = require("./app");
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Welcome to Backend !!');
+});  
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });

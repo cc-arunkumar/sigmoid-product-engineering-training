b45928d5df8c3880c017c0ee191f34cbb11c7db3
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-17 15:33:05 +0530

    feat(middleware): add application-level logging middleware

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const productRoutes = require("./routes/productRoutes");
+const logger=require("./middleware/logger")
 app.use(express.json());
 app.use(productRoutes);
+app.use(logger)
 app.listen(PORT, () => {
   return console.log(`Server running on port ${PORT}`);
 });

881cbbd5d91bcec7e10122f620f448b2ba18c7e5
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 13:14:20 +0530

    get product by Id

--- kalpana/week1-day1/src/controllers/productController.js
+++ kalpana/week1-day1/src/controllers/productController.js
@@ -3,3 +3,15 @@ let products = require("../data/products");
 exports.getAllProducts = (req,res) => {
     res.json(products);
 };
+
+exports.getProductById = (req,res) => {
+    const productId = parseInt(req.params.id);      // in request params are all the parameters from there select on eparameter id
+    const product = products.find(p => p.id === productId);
+
+    if(!product){
+        return res.status(404).json({
+            message : "Not Found!"
+        });
+    }
+    res.json(product);
+};
--- kalpana/week1-day1/src/routes/productRoutes.js
+++ kalpana/week1-day1/src/routes/productRoutes.js
@@ -4,4 +4,6 @@ const productController = require("../controllers/productController");
 
 router.get('/products', productController.getAllProducts);
 
+router.get("/product/:id", productController.getProductById);
+
 module.exports = router;

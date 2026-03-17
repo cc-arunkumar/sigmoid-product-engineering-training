f78a0221b58fdaee42a38260814962e0fc56abc7
AS-MSC-1310 <as-msc-1310@AS-MAC-1310.local>
2026-03-17 13:35:07 +0530

    feat(api): implement GET /products/:id

--- participants/siddhartha_m_h/week1-day1/controllers/productController.js
+++ participants/siddhartha_m_h/week1-day1/controllers/productController.js
@@ -4,7 +4,20 @@ function getAllProducts (req, res) {
     return res.json(products);
 }
 
+function getProductById (req, res) {
+    const id = parseInt(req.params.id);
+    const product = products.find(p => p.id === id); 
+
+    if (!product) {
+        return res.status(404).json({
+            message: "Product Not Found"
+        });
+    }
+
+    res.json(product);
+}
 
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductById
 }
--- participants/siddhartha_m_h/week1-day1/routes/product.js
+++ participants/siddhartha_m_h/week1-day1/routes/product.js
@@ -4,5 +4,6 @@ const productController = require('../controllers/productController')
 const router = express.Router();
 
 router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById)
 
 module.exports = router;

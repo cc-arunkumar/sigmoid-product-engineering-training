990b4f7e51e233905e999c5223a01350ec24a572
Shubham <220108059shubham@gmail.com>
2026-03-17 12:57:49 +0530

    added productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,78 @@
+const products = require("../data/products");
+// get all prouduct
+exports.getAllProducts = (req, res) => {
+    res.json(products);
+};
+
+// get product by id
+exports.getProductById = (req, res) =>{
+  const productId = parseInt(req.params.id);
+    const findProduct = products.find(p => p.id === productId);
+
+    if(!findProduct){
+       return res.status(404).json({
+        message : " product  not found"
+       })
+    }
+    res.json(findProduct);
+}
+
+// create new product
+ exports.createProduct = (req ,res)=>{
+   const {id , name , price , category , stock , brand} = req.body;
+
+   const newProduct = {
+    id : id,
+    name : name,
+    price : price,
+    category : category,
+    stock : stock,
+    brand : brand
+   }
+   products.push(newProduct);
+   res.status(201).json(newProduct);
+ }
+
+ // update product
+ 
+ exports.updateProduct = (req , res) =>{
+  const productId = parseInt(req.params.id);
+  const findProduct = products.find(p => p.id === productId);
+
+  if(!findProduct){
+    return res.status(404).json({
+      message: "Product not found"
+    })
+  }
+  const {id , name , price , category , stock , brand} = req.body;
+
+  findProduct.id = id,
+  findProduct.name = name,
+  findProduct.price = price,
+  findProduct.category = category,
+  findProduct.stock = stock,
+  findProduct.brand = brand
+
+  res.json(findProduct);
+ }
+// delete product
+exports.deleteProduct = (req , res) =>{
+  const productId = parseInt(req.params.id);
+
+  const findProductIndex = products.findIndex(p => p.id === productId);
+
+  if(findProductIndex == -1){
+    return res.status(404).json({
+      message: "Product not found"
+    })
+  } 
+  products.splice(findProductIndex,1);
+
+  res.json({
+    message: "Product deleted successfully"
+  })
+  }
+
+  // patch product
+  
+

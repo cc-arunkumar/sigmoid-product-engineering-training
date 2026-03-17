c62ce4402e532057c63d820ae3ed6812dc40b2b9
Anish <anish01022@gmail.com>
2026-03-17 13:14:12 +0530

    Implement GET /users/:id endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,3 +2,17 @@ const products = require("../data/products")
 exports.getALLProducts = (req, res) => {
     res.json(products);
 };
+exports.getProductById = (req , res) => {
+
+    const productid = parseInt(req.params.id);
+    
+    const product = products.find( p => p.id === productid);
+
+    if(!product){
+       return  res.status(404).json({
+            message: "Product Not Found"
+        })
+    }
+    res.json(product);
+
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,4 +2,5 @@ const express = require("express")
 const router = express.Router();
 const productController = require("../controllers/productController");
 router.get("/products" , productController.getALLProducts);
+router.get("/products/:id", productController.getProductById);
 module.exports = router;

50f2a4019cb35f111f07148cbe10ac80946e8ce8
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-17 13:02:01 +0530

    Get product by ID

--- anushtha/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ anushtha/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -2,4 +2,5 @@ const express = require("express")
 const router=express.Router();//have multiple paths from here
 const productController=require("../controllers/productController.js");
 router.get("/products",productController.getAllProducts);
+router.get("/products/:id",productController.getProductById);
 module.exports=router;

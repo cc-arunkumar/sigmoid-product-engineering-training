230a2ad720f520f3732e9b15d00fa1896efea5ba
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-17 16:30:08 +0530

    feat(middleware): add product validation middleware for POST, PUT and PATCH routes

--- Participants/NikhilCS/week1-day2/src/routes/productRoutes.js
+++ Participants/NikhilCS/week1-day2/src/routes/productRoutes.js
@@ -1,11 +1,13 @@
 //using default exports then go for this
 const express = require("express");
+const validator=require("../middleware/validateProduct")
+const patchvalidator=require("../middleware/validateProductPartial")
 const router = express.Router(); //created a router here we can create multiple routes now ,from here we can have multiple paths or routers now
 const productController = require("../controllers/productController"); //imported the controller here from controller file  ,this is one path
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/product/:id",productController.getProductById);
-router.post("/api/products",productController.createProduct)
-router.put("/api/product/:id",productController.updateProduct);
-router.patch("/api/product/:id",productController.updatePartialProduct);
+router.post("/api/products",validator,productController.createProduct)
+router.put("/api/product/:id",validator,productController.updateProduct);
+router.patch("/api/product/:id",patchvalidator,productController.updatePartialProduct);
 router.delete("/api/product/:id",productController.deleteProduct);
 module.exports=router

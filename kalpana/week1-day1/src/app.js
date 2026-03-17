739ea57cd413d8e30501340567f74eb3cb362076
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 13:11:53 +0530

    get all products

---
+++ kalpana/week1-day1/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const productRoutes = require("./routes/productRoutes");
+app.use(express.json());
+app.use(productRoutes);
+
+app.listen(3000, ()=>{
+    console.log("server is running....")
+})
+

b513246b13a458bb629bffae8c01912251b7e7db
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-17 16:19:18 +0530

    feat(middleware): add product validation middleware for POST, PUT and PATCH routes

--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const productController=require("../controllers/productControllers");
+const validateProduct = require('../middleware/validateProduct');
+const validateProductPartial = require('../middleware/validateProductPartial');
+
 router.get("/api/products",productController.getAllProducts)
 router.get("/api/product/:id",productController.getProductById)
-router.post("/api/products",productController.createProduct)
-router.put("/api/product/:id",productController.updateProduct)
-router.patch("/api/product/:id",productController.updateProductPartially)
+router.post("/api/products",validateProduct,productController.createProduct)
+router.put("/api/product/:id",validateProduct,productController.updateProduct)
+router.patch("/api/product/:id",validateProductPartial ,productController.updateProductPartially)
 router.delete("/api/product/:id",productController.deleteProduct)
+
 module.exports = router;

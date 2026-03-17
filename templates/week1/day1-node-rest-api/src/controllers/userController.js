64ebd29bbcb4208c769a6722b4d2e0a2c557eb49
AS-MAC-1230 <as-mac-1230@AS-MAC-1230s-MacBook-Air.local>
2026-03-17 12:53:21 +0530

    feat(api): implement GET /users/:id

--- templates/week1/day1-node-rest-api/src/controllers/userController.js
+++ templates/week1/day1-node-rest-api/src/controllers/userController.js
@@ -1,3 +1,7 @@
 exports.getAllUsers = (req, res) => {
   res.send("Get all users");
 };
+
+exports.getUserById = (req, res) => {
+  res.send(`Get user with id ${req.params.id}`);
+};
--- templates/week1/day1-node-rest-api/src/routes/userRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/userRoutes.js
@@ -2,10 +2,13 @@ const router = express.Router();
 
 const {
   getAllUsers,
+  getUserById,
 } = require("../controllers/userController");
 
 // Routes
 router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+
 
 
 module.exports = router;

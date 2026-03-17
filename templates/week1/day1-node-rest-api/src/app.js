db53e9d74d525fb181e524e2a304cbe7e7814676
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-17 13:25:23 +0530

    Connect product routes to main app

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -4,6 +4,8 @@ const app=express()
 const PORT=3000
 
 app.use(express.json());
+const productRoutes=require("./routes/productRoutes");
+app.use(productRoutes);
 app.listen(PORT,()=>{
     console.log("server running on Port 3000")
 });
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -1,10 +1,10 @@
 const express=require("express")
 const router=express.Router();
 const productController=require("../controllers/productController");
-router.get("/products",productController.getAllProducts);
-router.get("/products/:id",productController.getProductById);
-router.post("/products",productController.createProduct)
-router.put("/products/:id",productController.updateProduct)
-router.patch("/products/:id",productController.partialUpdateProduct)
-router.delete("/products/:id",productController.deleteProduct)
+router.get("/api/products",productController.getAllProducts);
+router.get("/api/products/:id",productController.getProductById);
+router.post("/api/products",productController.createProduct)
+router.put("/api/products/:id",productController.updateProduct)
+router.patch("/api/products/:id",productController.partialUpdateProduct)
+router.delete("/api/products/:id",productController.deleteProduct)
 module.exports=router;

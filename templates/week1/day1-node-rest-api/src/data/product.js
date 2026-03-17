625b52ccab6895fb91c0179b34631056bb85bb57
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-17 13:50:30 +0530

    feat(products) add products data

---
+++ templates/week1/day1-node-rest-api/src/data/product.js
@@ -0,0 +1,27 @@
+const products=[
+    {
+        product_id:1,
+        product_name:"headphone",
+        product_price:100000,
+        category:"Electronics",
+        Stock :5
+      },{
+          product_id:2,
+          product_name:"keyboard",
+          product_price:25000,
+          category: "Electronics",
+          Stock: 5
+      },{
+        product_id:3,
+        product_name:"iphone",
+        product_price:100000,
+        category: "Electronics",
+        Stock: 5
+      },{
+          product_id:4,
+          product_name:"samsung galaxy s11",
+          product_price:25000,
+          category: "Electronics",
+          Stock: 5
+      }];
+      module.exports=products;

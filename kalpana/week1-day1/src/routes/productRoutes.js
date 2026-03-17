e4e261e3875a875531de2e9ac557c15b4431597c
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 13:28:18 +0530

    Router path change in GET ALL PRODUCTS

--- kalpana/week1-day1/src/routes/productRoutes.js
+++ kalpana/week1-day1/src/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get('/products', productController.getAllProducts);
+router.get('api/products', productController.getAllProducts);
 
 router.get("/product/:id", productController.getProductById);
 

32c9dedbe5f525331ca27b5580ebc87feb282c13
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-17 13:48:39 +0530

    fixed all api endpoints to include /api at the beginning

--- Participants/NikhilCS/week1-day1/src/routes/productRoutes.js
+++ Participants/NikhilCS/week1-day1/src/routes/productRoutes.js
@@ -2,10 +2,10 @@
 const express = require("express");
 const router = express.Router(); //created a router here we can create multiple routes now ,from here we can have multiple paths or routers now
 const productController = require("../controllers/productController"); //imported the controller here from controller file  ,this is one path
-router.get("/products", productController.getAllProducts);
-router.get("/product/:id",productController.getProductById);
-router.post("/products",productController.createProduct)
-router.put("/product/:id",productController.updateProduct);
-router.patch("/product/:id",productController.updatePartialProduct);
-router.delete("/product/:id",productController.deleteProduct);
+router.get("/api/products", productController.getAllProducts);
+router.get("/api/product/:id",productController.getProductById);
+router.post("/api/products",productController.createProduct)
+router.put("/api/product/:id",productController.updateProduct);
+router.patch("/api/product/:id",productController.updatePartialProduct);
+router.delete("/api/product/:id",productController.deleteProduct);
 module.exports=router

615651bcd889abc8cc266f5834e67e4fa05259c4
nikhilkgupta2 <nikhilkumar@sigmoidanalytics.com>
2026-03-17 16:19:45 +0530

    added the validation part of the middlewere for the  post request

--- Routes/route.js
+++ Routes/route.js
@@ -3,10 +3,21 @@ const express = require("express") ;
 
 const router = express.Router() ; 
 
-const controller = require("../Controller/controller")
+const controller = require("../Controller/controller");
+const { validation } = require("../middlewere/logger");
+
 router.get("/api/products" , controller.getallProducts);
 router.get('/api/product/:id' ,  controller.getproductbyId);
-router.post("/api/productpost" , controller.postproduct)
+
+
+router.post(
+    "/api/productpost",
+    validation , 
+    controller.postproduct
+);
+
+
+
 router.put("/api/productput" , controller.putproduct);
 router.delete("/api/productdelete/:id" , controller.deleteproduct);
 

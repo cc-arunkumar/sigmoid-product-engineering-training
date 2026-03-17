41fb85209a869dbd103618ae9dd5b6adf20d82a9
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-17 13:28:49 +0530

    feat(api): implement GET /products

---
+++ templates/week1/day1-node-rest-api/src/data/products.js
@@ -0,0 +1,29 @@
+const product = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 100000.00,
+        category: "Electronics",
+        Stock : 10
+    },
+    {
+        id: 2,
+        name: "Mobile",
+        price: 25000.00,
+        category: "Electronics",
+        Stock : 10
+    },
+   { 
+        id: 3,
+        name: "Tablet",
+        category: "Electronics",
+        Stock : 8
+    },
+    {
+        id: 4,
+        name: "Headphones",
+        category: "Electronics",
+        Stock : 5
+    },
+] 
+module.exports = product;
---
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productControllers');
+
+
+router.get('/products', productController.getAllProducts);
+
+module.exports = router;

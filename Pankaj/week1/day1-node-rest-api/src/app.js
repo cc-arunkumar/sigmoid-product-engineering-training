2ba49d30cfe5fc1770f8d63345586490d421d540
AS-MAC-1339 <as-mac-1339@AS-MAC-1339s-MacBook-Air.local>
2026-03-17 17:44:13 +0530

    feat(middleware): add centralized error handling middleware

--- Pankaj/week1/day1-node-rest-api/src/app.js
+++ Pankaj/week1/day1-node-rest-api/src/app.js
@@ -5,12 +5,17 @@ const app = express();
 const productRoutes = require("./routes/productRoutes");
 
 const logger = require("./middleware/logger");
+const e = require("express");
+const errorhandler = require("./middleware/errorHandler");
+const errorHandler = require("./middleware/errorHandler");
 app.use(express.json());
 
 
 app.use(logger);
 
 app.use(productRoutes);
+
+app.use(errorHandler);
 
 app.get("/api", (req, res) => {
     res.send("Welcome to the Backend !!");

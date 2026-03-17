414ed47ad9f00851f0a28f5c095342d60996ce64
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-17 13:38:10 +0530

    feat(api): implement GET /product/:id

--- Participants/NikhilCS/week1-day1/src/controllers/productController.js
+++ Participants/NikhilCS/week1-day1/src/controllers/productController.js
@@ -3,3 +3,22 @@ const products = require("../data/products");
 exports.getAllProducts = (req, res) => {
   res.json(products);
 };
+exports.getProductById = (req, res) => {
+  //const productId=parseInt(req.params.id)
+  //parseInt however has a drawbakc convert "100a" into 100 which is incorrect so use the Number method instead
+  const productId = Number(req.params.id);
+  console.log(productId);
+  const product = products.find((product) => {
+    return product.id === productId;
+  }); //if we search or find here for a non exisent ided object that is an object without the required id ,so non existent product then emptty object ,product has non data response then we go for the status code of 404 being sent
+  if (!product) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+    //we could do
+    //return res.status(404).send({
+    //         message:"Product not found"
+    // })
+  }
+  res.json(product);
+};

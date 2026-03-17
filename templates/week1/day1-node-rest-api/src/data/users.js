7b7e5fcdab439b08f6f6be0c47c45fada7d7ec9c
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-17 12:14:25 +0530

    chore(project): initialize Node project

---
+++ templates/week1/day1-node-rest-api/src/data/users.js
@@ -0,0 +1,8 @@
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com"
+    }
+];
+module.exports = users;
--- templates/week1/day1-node-rest-api/src/routes/userRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/userRoutes.js
@@ -2,18 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
+  getAllUsers
 } = require("../controllers/userController");
 
 // Routes
 router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-router.post("/", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+
 
 module.exports = router;

524a09b0b212874ab33de9c8b05e20a5e7869e72
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-17 13:05:26 +0530

    Add products(Create product)

--- anushtha/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ anushtha/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -3,4 +3,5 @@ const router=express.Router();//have multiple paths from here
 const productController=require("../controllers/productController.js");
 router.get("/products",productController.getAllProducts);
 router.get("/products/:id",productController.getProductById);
+router.post("/products",productController.createProduct);
 module.exports=router;

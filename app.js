6d1e557ffaff2c424f7a1797d286a02972b6cdbf
AS-MAC-1209 <as-mac-1209@AS-MAC-1209s-MacBook-Air.local>
2026-03-17 15:21:10 +0530

    feat(middleware): add application-level logging middleware

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const PORT = 3000
 
 const productRoutes = require("./routes/productRoutes");
 
+const logger = require("./middleware/logger")
+
 app.use(express.json()); // this is a middleware that converts JSON format data into javascript object (node can read the javascript object not JSON)
+app.use(logger)
 app.use(productRoutes);
 
 app.listen(PORT, () => {

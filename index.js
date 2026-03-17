14c1339a0187d0d66e32acc884dea439ccef5649
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-03-17 15:27:51 +0530

    feat(middleware): add application-level logging middleware

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 
 const productRoutes = require("./routes/productRoutes")
+const logger = require("./middleware/logger")
 
 app.use(express.json())
+app.use(logger)
 app.use(productRoutes)
 
 app.listen(3000, () => {

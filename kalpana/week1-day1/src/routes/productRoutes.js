9196b672ec7646a43aea009b3bff4d18c6d0cbcf
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-17 13:32:49 +0530

    Router path change

--- kalpana/week1-day1/src/routes/productRoutes.js
+++ kalpana/week1-day1/src/routes/productRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get('api/products', productController.getAllProducts);
+router.get('/api/products', productController.getAllProducts);
 
-router.get("api/product/:id", productController.getProductById);
+router.get("/api/product/:id", productController.getProductById);
 
-router.post("api/products", productController.createProduct);
+router.post("/api/products", productController.createProduct);
 
-router.put("api/product/:id", productController.updateProduct);
+router.put("/api/product/:id", productController.updateProduct);
 
-router.delete("api/product/:id", productController.deleteProductById);
+router.delete("/api/product/:id", productController.deleteProductById);
 
-router.patch("api/product/:id", productController.updatePartialProduct);
+router.patch("/api/product/:id", productController.updatePartialProduct);
 
 module.exports = router;

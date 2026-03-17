7eb58518be0e4d0174fd1a44a2124bdd4f64eeab
AS-MAC-1225 <as-mac-1225@AS-MAC-1225.local>
2026-03-17 17:33:57 +0530

    feat(middleware): add centralized error handling middleware

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express=require("express");
 const app=express();
 const productRoutes = require("./routes/productRoutes");
 const logger=require("./middleware/logger");
+const errorHandler=require("./middleware/errorHandler");
 app.use(express.json());
 app.use(logger);
 app.use("/",productRoutes);
+app.use(errorHandler);
 app.listen(3000,()=>{
     console.log("Server running on port 3000")
 });

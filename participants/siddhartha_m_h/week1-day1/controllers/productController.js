9b7fcaefdd0e39a3fa792e940f0e63a0b949aee9
AS-MSC-1310 <as-msc-1310@AS-MAC-1310.local>
2026-03-17 13:20:10 +0530

    Get all products

---
+++ participants/siddhartha_m_h/week1-day1/controllers/productController.js
@@ -0,0 +1,5 @@
+const products = require('../data/product')
+
+function getAllProducts (req, res) {
+    return res.json(products);
+}
---
+++ participants/siddhartha_m_h/week1-day1/data/product.js
@@ -0,0 +1,34 @@
+const products = [
+        {
+            id : 101,
+            name : 'Laptop',
+            price : 100000.00,
+            category : "Electronics",
+            stock : 25
+        },
+        {
+            id : 102,
+            name : 'Mobile',
+            price : 25000.00,
+            category : "Electronics",
+            stock : 2
+        },
+        {
+            id : 103,
+            name : 'Headphone',
+            price : 10000.00,
+            category : "Electronics",
+            stock : 43
+        },
+        {
+            id : 104,
+            name : 'Keyboard',
+            price : 5000.00,
+            category : "Electronics",
+            stock : 15
+        }
+]
+
+module.exports = products
+
+    

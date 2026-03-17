23937a788109c6d46d78f121424ed631e4dad6ad
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-17 12:57:47 +0530

    feat(api): implement PATCH /users/:id

--- templates/week1/day1-node-rest-api/src/controllers/userController.js
+++ templates/week1/day1-node-rest-api/src/controllers/userController.js
@@ -4,3 +4,68 @@ exports.getAllUsers = (req, res) => {
     res.json(users);
 }
 
+exports.getUserById = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if(!user){
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+}
+
+exports.createUser = (req, res) => {
+    const { name, email } = req.body;
+    const newUser = {
+        id: Date.now(),
+        name,
+        email
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+}
+
+exports.updateUser = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if(!user){
+        return res.status(404).json({ message: 'User not found' });
+    }
+   else{
+    const { name, email } = req.body;
+    user.name = name || user.name;
+    user.email = email || user.email;
+
+    res.status(200).json(user);
+
+}
+}
+
+exports.deleteUser = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === userId);
+
+    if(userIndex === -1){
+        return res.status(404).json({ message: 'User not found' });
+    }
+    users.splice(userIndex, 1);
+    res.status(204).send();
+}
+
+exports.updatePartialUser = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if(!user){
+        return res.status(404).json({ message: 'User not found' });
+    }
+   else{
+    const { name, email } = req.body;
+    if(name) user.name = name;
+    if(email) user.email = email;
+
+    res.status(200).json(user);
+
+}
+}
--- templates/week1/day1-node-rest-api/src/routes/userRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/userRoutes.js
@@ -9,7 +9,21 @@ const { routes } = require("../app");
 // Routes
 
 routes.get("/users", getAllUsers);
-
+router.get("/users/:id", (req, res) => {
+  res.send("Get User by ID");
+});
+router.post("/users", (req, res) => {
+  res.send("Create User");
+});
+router.put("/users/:id", (req, res) => {
+  res.send("Update User");
+});
+router.delete("/users/:id", (req, res) => {
+  res.send("Delete User");
+});
+router.patch("/users/:id", (req, res) => {
+  res.send("Update Partial User");
+});
 
 
 module.exports = router;

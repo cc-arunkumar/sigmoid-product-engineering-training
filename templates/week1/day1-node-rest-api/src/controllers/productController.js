4759bf38a704147eb353fceee61bee10fef0d662
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-17 13:13:58 +0530

    Add API and route to update product by ID

--- templates/week1/day1-node-rest-api/src/controllers/productController.js
+++ templates/week1/day1-node-rest-api/src/controllers/productController.js
@@ -28,3 +28,20 @@ exports.createProduct=(req,res)=>{
     message:"product added successfully"
    })
 }
+
+exports.updateProduct=(req,res)=>{
+    const productId=req.params.id*1;
+    const product=products.find(p=>p.id===productId)
+    if(!product)
+    {
+        return res.status(404).json({
+            message:"not found"
+        })
+    }
+    const {name,price,category,stock}=req.body;
+    product.name=name;
+    product.price=price;
+    product.category=category;
+    product.stock=stock;
+    res.status(200).json(product)
+}
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -4,3 +4,4 @@ const productController=require("../controllers/productController");
 router.get("/products",productController.getAllProducts);
 router.get("/products/:id",productController.getProductById);
 router.post("/products",productController.createProduct)
+router.put("/products/:id",productController.updateProduct)

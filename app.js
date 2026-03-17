1111724d7f9173994221c086132f26c7e3cbf916
Sai Ganesh <as-mac-1228@AS-MAC-1228.local>
2026-03-17 13:11:38 +0530

    Get Products

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ app.get("/",(req,res)=>{
     res.send("Welcome");    
 })
 
+app.get("/products",(req,res)=>{
+    const products=[
+        {
+            id:102,name:"Keyboard",price:25000.00
+        },
+        {
+            id:101,name:"Laptop",price:100000.00
+        },
+        {
+            id:103,name:"Headphones",price:60000.00
+        },
+        {
+            id:104,name:"Mobile",rice:90000.00
+        }
+    ]
+    res.json(products) 
+})
+
 app.listen(3000, () => {     
     console.log("Yes")
 })

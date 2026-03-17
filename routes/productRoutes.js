998d35a13cb813d458cbba310fee42e3039b0c7b
Sai Ganesh <as-mac-1228@AS-MAC-1228.local>
2026-03-17 13:26:29 +0530

    feat(api): implement GET /users/:id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,7 @@ const router=express.Router();
 
 const productController=require("../controllers/productController.js")
 
-router.get("/products",productController.getAllProducts);
+router.get("/api/products",productController.getAllProducts);
+router.get("/api/products/:id",productController.getProductById);
 
 module.exports=router;

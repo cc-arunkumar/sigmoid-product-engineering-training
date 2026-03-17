27a7e2cd3c6829e091acddde8e2645c5b0634ce1
AS-MAC-1339 <as-mac-1339@AS-MAC-1339s-MacBook-Air.local>
2026-03-17 16:00:24 +0530

    feat(middleware): add product validation middleware for POST, PUT and PATCH routes

---
+++ Pankaj/week1/day1-node-rest-api/src/middleware/validateProduct.js
@@ -0,0 +1,31 @@
+const validateProduct = (req, res, next)=> {
+    const {name, price, category, stock} = req.body;
+
+    if(!name || name.trim()==="" || name.length == 0){
+        return res.status(400).json({
+            success: false,
+            message: "Name is required and should not be empty"});
+    }
+
+    if(price === undefined || price <= 0){
+        return res.status(400).json({
+            success: false,
+            message: "Price is required and should be a positive number"
+        });
+    }
+    if(!category || category.trim() === "" || category.length == 0){
+        return res.status(400).json({
+            success: false,
+            message: "Category is required and should not be empty"
+        });
+    }
+    if(stock === undefined || stock < 0){
+        return res.status(400).json({
+            success: false,
+            message: "Stock is required and should be a non-negative number"
+        });
+    }
+    next();
+}
+
+module.exports = validateProduct;
--- Pankaj/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ Pankaj/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productControllers');
+const validateProduct = require('../middleware/validateProduct');
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id", productController.getProductById);
-router.post("/api/products", productController.createProduct);
+router.post("/api/products", validateProduct, productController.createProduct);
 router.put("/api/products/:id", productController.updateProduct);
 router.delete("/api/products/:id", productController.deleteProduct);
 router.patch("/api/products/:id", productController.updatePartialProduct);

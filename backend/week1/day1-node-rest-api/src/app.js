51b6bd6198b01ba40c73a1a69da86e764e3dfd0a
mohammadrustam-sigmoid <mohammad.rustam@sigmoidanalytics.com>
2026-03-17 15:20:37 +0530

    feat(middleware): add application-level logging middleware

--- backend/week1/day1-node-rest-api/src/app.js
+++ backend/week1/day1-node-rest-api/src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
+const { logger } = require("./middleware/logger");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(logger);
 // Register routes
 app.use("/api/v1/products", productRoutes);
 

249c022395c516342bf4a855b8d58627020e50be
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-03-17 14:35:24 +0530

    feat: add PATCH /products/:id to partially update a product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,6 +9,6 @@ router.get("/api/product/:id", productController.getProductById);
 router.post("/api/products", productController.createProduct);
 router.put("/api/products/:id", productController.updateProduct);
 router.delete("/products/:id", productController.deleteProduct);
-
+router.patch("/products/:id", productController.updatePartialProduct);
 
 module.exports = router;

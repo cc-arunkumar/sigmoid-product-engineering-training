1fcf2454b9b9fcc99f0301ff333ddccf8867161a
AS-MAC-1116 <as-mac-1116@AS-MAC-1116.local>
2026-03-17 13:18:39 +0530

    feat(api): implement GET /products

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,11 +5,3 @@ exports.getProducts=(req,res)=>{
     res.json(products);
 };
 
-exports.getProductById=(req,res)=>{
-    const pId=parseInt(req.params.id);
-    const product=products.find(p=>p.id===pId);
-    if(!product){
-        return res.status(404).json({message:"Product not found"});
-    }
-    res.send(product);
-};

1f330e8cd24fcf183400b68495a91108e2bbb31b
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-17 16:29:39 +0530

    feat(middleware): add partial validation middleware for PATCH product updates

--- anushtha/week1/day2-node-rest-api/src/routes/productRoutes.js
+++ anushtha/week1/day2-node-rest-api/src/routes/productRoutes.js
@@ -2,10 +2,11 @@ const express = require("express")
 const router=express.Router();//have multiple paths from here
 const productController=require("../controllers/productController");
 const validateProduct=require("../middleware/validateProduct");
+const validatePartialProduct=require("../middleware/validatePartialProduct");
 router.get("/api/products",productController.getAllProducts);
 router.get("/api/products/:id",productController.getProductById);
 router.post("/api/products",validateProduct,productController.createProduct);
 router.put("/api/products/:id",validateProduct,productController.updateProduct);
 router.delete("/api/products/:id",productController.deleteProduct);
-router.patch("/api/products/:id",validateProduct,productController.updatePartialProduct);
+router.patch("/api/products/:id",validatePartialProduct,productController.updatePartialProduct);
 module.exports=router;

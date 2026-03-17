e64ed3b5c0285720bccda0149355c80d2deec50c
AS-MAC-1339 <as-mac-1339@AS-MAC-1339s-MacBook-Air.local>
2026-03-17 13:04:33 +0530

    Initializing Server Setup

--- templates/week1/day1-node-rest-api/src/server.js
+++ templates/week1/day1-node-rest-api/src/server.js
@@ -1,7 +1,11 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} `);
+app.listen(3000, () => {
+  console.log(`Server running on port ${PORT}`);
 });

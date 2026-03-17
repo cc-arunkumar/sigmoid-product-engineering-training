6ecbe4d9f2d4e522bb4a09ec33f7f99d0d58b1b1
AS-MAC-1339 <as-mac-1339@AS-MAC-1339s-MacBook-Air.local>
2026-03-17 13:18:10 +0530

    Initializing Server Setup

--- templates/week1/day1-node-rest-api/src/controllers/productControllers.js
+++ templates/week1/day1-node-rest-api/src/controllers/productControllers.js
@@ -3,3 +3,13 @@ const products = require("../data/products");
 exports.getAllProducts = (req, res) => {
     res.json(products);
 };
+
+exports.getProductById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = products.find(p => p.id === id);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ message: "Product not found" });
+    }
+};  

70f285b87a54b559904002e8b3082ee7e33e6219
mohammadrustam-sigmoid <mohammad.rustam@sigmoidanalytics.com>
2026-03-17 12:32:24 +0530

    welcome to Backend

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -1,11 +1,1 @@
-const express = require("express");
-const userRoutes = require("./routes/userRoutes");
 
-const app = express();
-
-app.use(express.json());
-
-// Register routes
-app.use("/api/users", userRoutes);
-
-module.exports = app;

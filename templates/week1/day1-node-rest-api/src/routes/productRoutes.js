e8d9b95ca931da8e0469e6e3730e30ad523296f3
AS-MAC-1150 <as-mac-1150@AS-MAC-1150s-MacBook-Air.local>
2026-03-17 13:32:07 +0530

    feat(api): implement GET /products/:id

--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const productControllers = require("../controllers/productControllers")
 
 router.get("/products", productControllers.getAllProducts);
+router.get("/products/:id", productControllers.getProductById);
 
 module.exports = router;

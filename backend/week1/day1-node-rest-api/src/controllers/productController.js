6d5641bc0a55cffb0872218611066ec731c9043f
mohammadrustam-sigmoid <mohammad.rustam@sigmoidanalytics.com>
2026-03-17 14:44:52 +0530

    feat(api): implement PATCH /prodcuts/:id

--- backend/week1/day1-node-rest-api/src/controllers/productController.js
+++ backend/week1/day1-node-rest-api/src/controllers/productController.js
@@ -29,3 +29,35 @@ exports.createPost = (req, res) => {
     data: newProduct,
   });
 };
+
+exports.updatePost = (req, res) => {
+  const id = req.params.productId * 1;
+  const { name, price, stock, category } = req.body;
+
+  let pr = products.find((ele) => ele.id === id);
+
+  pr.name = name;
+  pr.price = price;
+  pr.stock = stock;
+  pr.category = category;
+
+  res.status(201).json({
+    message: "product updated successfully",
+    data: pr,
+  });
+};
+
+exports.editPost = (req, res) => {
+  const id = req.params.productId * 1;
+
+  let pr = products.find((ele) => ele.id === id);
+
+  Object.keys(req.body).forEach((key) => {
+    pr[key] = req.body[key];
+  });
+
+  res.status(201).json({
+    message: "product edited successfully",
+    data: pr,
+  });
+};
--- backend/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ backend/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -7,5 +7,7 @@ const productController = require("../controllers/productController");
 router.get("/", productController.getAllproducts);
 router.get("/:productId", productController.getProductsById);
 router.post("/", productController.createPost);
+router.put("/:productId", productController.updatePost);
+router.patch("/:productId", productController.editPost);
 
 module.exports = router;

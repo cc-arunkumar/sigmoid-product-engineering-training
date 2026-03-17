69529c9e6694c721e16c2f4981b5116fc0555d81
AS-MAC-1348 <as-mac-1313@AS-MAC-1348.local>
2026-03-17 13:14:05 +0530

    feat(api): implement GET /products

--- app.js
+++ app.js
@@ -6,6 +6,42 @@ app.get("/",(req,res)=>{
      res.send("Hello World i am yuvraj singh hiii");
  });
 
+
+ app.get("/",(req,res)=>{
+    res.send("Hello World i am yuvraj singh hiii");
+})
+
+
+app.get("/products",(req,res) => {
+    const products = [
+        {
+            id:101,
+            name:"laptop",
+            price:50000
+        },
+        {
+            id:101,
+            name:"Mobile",
+            price:100000
+        },
+        {
+            id:101,
+            name:"Headphones",
+            price:160000
+        },
+        {
+            id:101,
+            name:"Keyboard",
+            price:1500000
+        }
+    ]
+    res.json(products)
+
+});
+
+
+
+
  app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })

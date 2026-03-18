cd08a67ef0ff09e8f01193fad47176a7dadfb40c
pranavky12-sigmoid <pranav.kumar@sigmoidanalytics.com>
2026-03-18 17:40:52 +0530

    feat(auth): implement role-based authorization middleware (RBAC)

--- participants/pranav/week1-day3/routes/productRoutes.js
+++ participants/pranav/week1-day3/routes/productRoutes.js
@@ -5,15 +5,19 @@ const router= express.Router()
 const productController=require("../controllers/productControllers")
 const validateProduct = require("../middleware/validateProduct")
 const validatePatchProduct = require("../middleware/validatePatchProduct")
+const protect = require("../middleware/authMiddleware")
+const authorize = require("../middleware/authorize")
 
 
 router.get("/",productController.getAllProducts)
       .get("/:id",productController.getProductById)
-       .post("/",validateProduct,productController.createProduct)
-       .put("/:id",validateProduct,productController.updateProduct)
-       .patch("/:id",validatePatchProduct, productController.patchProduct)
-       .delete("/:id",productController.deleteProduct)
+      .post("/",protect,authorize("user"),validateProduct,productController.createProduct)
+      .put("/:id",validateProduct,authorize("user"),productController.updateProduct)
+      .patch("/:id",protect,validatePatchProduct, authorize("admin"),productController.patchProduct)
+      .delete("/:id",protect,authorize("admin"),productController.deleteProduct)
 
 
 module.exports= router
- 
+
+
+

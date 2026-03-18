6191da3ed1b2fb05b0e8793a425243601d554d86
siyakalra-work <siya.kalra@sigmoidanalytics.com>
2026-03-18 16:09:03 +0530

    feat(auth): add JWT middleware and protect product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const validateProduct = require("../middleware/validateProduct");
 const validateProductPatch = require("../middleware/validateProductPatch");
+const protect=require("../middleware/authMiddleware");
 
 // GET
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id", productController.getProductById);
 
 // POST
-router.post("/api/products", validateProduct, productController.createProduct);
+router.post("/api/products",protect, validateProduct, productController.createProduct);
 
 // PUT
 router.put("/api/products/:id", validateProduct, productController.updateProduct);

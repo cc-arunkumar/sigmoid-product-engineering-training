89f0a7ebdba1585990f634ec140a0ab56aeac96d
AS-MAC-1197 <as-mac-1197@AS-MAC-1197s-MacBook-Air.local>
2026-03-18 16:49:48 +0530

    feat(auth): add JWT middleware and protect product routes

--- Ananya/week1/day2-node-rest-api/src/routes/productRouter.js
+++ Ananya/week1/day2-node-rest-api/src/routes/productRouter.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const productController = require("../controllers/productControllers");
 const validateProduct= require("../middleware/validateProduct");
 const validatePartialProduct= require("../middleware/validatePartialProduct");
+const protect= require("../middleware/authMiddleware");
 
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id", productController.getProductById );
-router.post("/api/products",validateProduct,productController.createProduct);
-router.put("/api/products/:id",validateProduct,productController.updateP);
-router.delete("/api/products/:id",productController.deleteP);
-router.patch("/api/products/:id",validatePartialProduct,productController.patchP );
+router.post("/api/products",protect,validateProduct,productController.createProduct);
+router.put("/api/products/:id",protect,validateProduct,productController.updateP);
+router.delete("/api/products/:id",protect,productController.deleteP);
+router.patch("/api/products/:id",protect,validatePartialProduct,productController.patchP );
 
 
 module.exports= router;

ccd5328602965b5090065853fd5f3f7feac59188
ankit17082 <ankitkumar@sigmoidanalytics.com>
2026-03-18 16:11:22 +0530

    feat(auth): add JWT middleware and protect product routes

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -9,10 +9,11 @@ const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());
 app.use(logger);
-app.use(errorHandler);
 
 app.use(productRoutes);
 app.use(authRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -5,12 +5,13 @@ const router = express.Router();
 const productControllers = require("../controllers/productControllers");
 const validateProduct = require("../middleware/validateProduct");
 const validateProductPartial = require("../middleware/validateProductPartial");
+const protect = require("../middleware/authMiddleware");
 
 router.get("/products", productControllers.getAllProducts);
 router.get("/products/:id", productControllers.getProductById);
-router.post("/products", validateProduct, productControllers.createProduct);
-router.put("/products/:id", validateProduct, productControllers.updateProduct);
-router.delete("/products/:id", productControllers.deleteProduct);
-router.patch("/products", validateProductPartial, productControllers.patchProduct);
+router.post("/products", protect, validateProduct, productControllers.createProduct);
+router.put("/products/:id", protect, validateProduct, productControllers.updateProduct);
+router.delete("/products/:id", protect, productControllers.deleteProduct);
+router.patch("/products", protect, validateProductPartial, productControllers.patchProduct);
 
 module.exports = router;

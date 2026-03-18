f5b16b297185287ef2cffe511e64888a78f1d7f9
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-18 11:32:49 +0530

    implementef user CRUD Operations

---
+++ saurabh/week1/day2-node-rest-api/src/data/user.js
@@ -0,0 +1,26 @@
+const users = [{
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    age: 30
+},
+{
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    age: 25
+},
+{
+    id: 3,
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    age: 35
+},
+{
+    id: 4,
+    name: "Alice Brown",
+    email: "alice.brown@example.com",
+    age: 28
+}];
+
+module.exports = users;

0d66e92ea9d13cae07bc04cedd034540f83b1053
AS-MAC-1161 <as-mac-@AS-MAC-1161s-MacBook-Air.local>
2026-03-18 16:27:11 +0530

    feat(auth): add JWT middleware and protect product routes

---
+++ templates/week1/day1-node-rest-api/src/middleware/middleauth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+    const AppError = require("../utils/AppError");
+
+    const protect = (req, res, next) => {
+        try{
+            let token;
+
+            if(
+                req.headers.authorization &&
+                req.headers.authorization.startsWith("Bearer ")
+            ) {
+                token = req.headers.authorization.split(" ")[1];
+            }
+
+            if (!token){
+                return next(new AppError("Access denied. No token provided", 401));
+            }
+
+            const decoded = jwt.verify(
+                token,
+                process.env.JWT_SECRET || "mysecretkey"
+            );
+
+            req.user = decoded;
+
+            next();
+        } catch (error){
+            if(error.name === "JsonWebTokenError"){
+                return next(new AppError("Invalid token", 401));
+            }
+
+            if (error.name === "TokenExpiredError"){
+                return next(new AppError("Token expired", 401));
+            }
+            return next(new AppError("Authentication failed", 401));
+        }
+    };
+
+    module.exports = protect;
--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -6,11 +6,12 @@ const router = express.Router(); //for multiple paths
 const productController = require("../controllers/productController");
 const validateProduct = require("../middleware/validateProduct");
 const validatePartialProduct = require("../middleware/validatePartialProduct");
+const protect = require("../middleware/middleauth");
 
 router.get("/products",productController.getAllProducts);
 router.get("/product/:id",productController.getProductById);
-router.post("/products",validateProduct,productController.createProduct);
-router.put("/product/:id",validateProduct,productController.updateProduct);
-router.delete("/product/:id",productController.deleteProduct);
-router.patch("/product/:id",validatePartialProduct,productController.patchProduct);
+router.post("/products",protect,validateProduct,productController.createProduct);
+router.put("/product/:id",protect,validateProduct,productController.updateProduct);
+router.delete("/product/:id",protect,productController.deleteProduct);
+router.patch("/product/:id",protect,validatePartialProduct,productController.patchProduct);
 module.exports=router;

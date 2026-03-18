4f557ff1120cfa41d17d3bfcb307c6cb8d3f80c5
Aditya <aditya.kumar26@sigmoidanalytics.com>
2026-03-18 16:14:58 +0530

    feat(auth): add JWT middleware and protect product routes

--- week1-day3/routes/productRoutes.js
+++ week1-day3/routes/productRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const { updateProduct, deleteProduct, getAllProducts, getProductById, createProduct, patchProduct } = require("../controllers/productControllers");
 const { validateProduct } = require("../middleware/validateProduct");
 const { validatePatchProduct } = require("../middleware/validatePatchProduct");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/products", getAllProducts);
 router.get("/product/:id", getProductById);
-router.post("/product", validateProduct, createProduct);
-router.put("/product/:id", validateProduct, updateProduct);
-router.patch("/product/:id", validatePatchProduct, patchProduct);
-router.delete("/product/:id", deleteProduct);
+router.post("/product", protect, validateProduct, createProduct);
+router.put("/product/:id", protect, validateProduct, updateProduct);
+router.patch("/product/:id", protect, validatePatchProduct, patchProduct);
+router.delete("/product/:id", protect, deleteProduct);
 
 module.exports = router;

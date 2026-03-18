be660e17f8bfed586bb622549b41e4e7b6715c4f
Utkarsh Kumar <utkarsh.kumar@sigmoidanalytics.com>
2026-03-18 11:45:24 +0530

    refactor(controller): replace errorResponse with next() for centralized error handlin

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,55 +1,97 @@
 import { products } from "../data/product_data.js";
 import { sucessResponse, errorResponse } from "../utils/apiResponse.js";
 function getAllProducts(req,res){
-    sucessResponse(res, products)
+    try {
+        sucessResponse(res, products)
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to fetch products"
+        })
+    }
 }
 function getProductById(req, res){
-    const productId=parseInt(req.params.id)
-    const product=products.find(p=>p.id===productId)
-    if(!product) return errorResponse(res, "Product not found", 404)
-    else return sucessResponse(res, product)
+    try {
+        const productId=parseInt(req.params.id)
+        const product=products.find(p=>p.id===productId)
+        if(!product) return errorResponse(res, "Product not found", 404)
+        else return sucessResponse(res, product)
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to fetch product"
+        })
+    }
 }
 function createProduct(req, res){
-    const {name,price,category,stock}=req.body
-    const newProduct={
-        id: products.length+1,
-        name:name,
-        price:price,
-        category:category,
-        stock:stock
+    try {
+        const {name,price,category,stock}=req.body
+        const newProduct={
+            id: products.length+1,
+            name:name,
+            price:price,
+            category:category,
+            stock:stock
+        }
+        products.push(newProduct)
+        sucessResponse(res, newProduct, "Product created successfully", 201)
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to create product"
+        })
     }
-    products.push(newProduct)
-    sucessResponse(res, newProduct, "Product created successfully", 201)
 }
 function updateProduct(req, res){
-    const productId=parseInt(req.params.id)
-    const product=products.find(p=>p.id===productId)
-    if(!product) return errorResponse(res, "Product not found", 404)
-    const {name,price,category,stock}=req.body
-    product.name=name
-    product.price=price
-    product.category=category
-    product.stock=stock
-    sucessResponse(res, product)
+    try {
+        const productId=parseInt(req.params.id)
+        const product=products.find(p=>p.id===productId)
+        if(!product) return errorResponse(res, "Product not found", 404)
+        const {name,price,category,stock}=req.body
+        product.name=name
+        product.price=price
+        product.category=category
+        product.stock=stock
+        sucessResponse(res, product)
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to update product"
+        })
+    }
 }
 function deleteProduct(req, res){
-    const productId=parseInt(req.params.id)
-    const product_index=products.findIndex(p=>p.id===productId)
-    if(product_index===-1){
-        return errorResponse(res, "Product not found", 404)
+    try {
+        const productId=parseInt(req.params.id)
+        const product_index=products.findIndex(p=>p.id===productId)
+        if(product_index===-1){
+            return errorResponse(res, "Product not found", 404)
+        }
+        products.splice(product_index,1)
+        return sucessResponse(res, null, "Product deleted successfully")
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to delete product"
+        })
     }
-    products.splice(product_index,1)
-    return sucessResponse(res, null, "Product deleted successfully")
 }
 function partialUpdate(req,res){
-    const productId=parseInt(req.params.id)
-    const product=products.find(p=>p.id===productId)
-    if(!product) return errorResponse(res, "Product not found", 404)
-    const {name,price,category,stock}=req.body
-    if(name) product.name=name
-    if(price) product.price=price
-    if(category) product.category=category
-    if(stock) product.stock=stock
-    return sucessResponse(res, product)
+    try {
+        const productId=parseInt(req.params.id)
+        const product=products.find(p=>p.id===productId)
+        if(!product) return errorResponse(res, "Product not found", 404)
+        const {name,price,category,stock}=req.body
+        if(name) product.name=name
+        if(price) product.price=price
+        if(category) product.category=category
+        if(stock) product.stock=stock
+        return sucessResponse(res, product)
+    } catch (error) {
+        return next({
+            status:500,
+            message:"Failed to update product"
+        })
+    }
 }
 export {getAllProducts,getProductById,createProduct,updateProduct,deleteProduct,partialUpdate}

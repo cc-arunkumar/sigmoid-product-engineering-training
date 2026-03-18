5dcc32b17bbc62ada826189d58531bea1b3152a4
cs-ganesh <chanda.sai@sigmoidanalytics.com>
2026-03-18 14:11:31 +0530

    Create productRoutes.js

---
+++ Day-3/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express=require("express")
+
+const router=express.Router();
+
+const productController=require("../controllers/productController.js");
+const validateProduct = require("../middleware/validateProduct.js");
+const validateProductPartial=require("../middleware/validateProductPartial.js");
+
+router.get("/api/products",productController.getAllProducts);
+router.get("/api/products/:id",productController.getProductById);
+router.post("/api/products", validateProduct ,productController.createProduct);
+router.put("/api/products/:id", validateProduct,productController.updateProduct); 
+router.delete("/api/products/:id",productController.deleteProduct);
+router.patch('/api/products/:id', validateProductPartial ,productController.patchProduct);
+
+module.exports=router;

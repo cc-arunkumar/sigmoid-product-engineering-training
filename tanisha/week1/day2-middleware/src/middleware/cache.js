54b767856600783371b768b7b01632a2c21c8bc9
AS-MAC-1154 <as-mac-1154@AS-MAC-1154s-MacBook-Air.local>
2026-03-18 18:18:37 +0530

    feat(performance): add response caching middleware for GET endpoints

---
+++ tanisha/week1/day2-middleware/src/middleware/cache.js
@@ -0,0 +1,24 @@
+const cacheStore=new Map();
+const cache =(duration)=>{
+    return (req,res,next)=>{
+        if(req.method!=="GET"){
+            return next();
+        }
+        const key = req.originalUrl;
+        const cachedData=cacheStore.get(key);
+        if(cachedData&& cachedData.expiry>Date.now()){
+            return res.status(200).json(cachedData.data);
+        }
+        const originalJson=res.json.bind(res);
+        res.json=(data)=>{
+            cacheStore.set(key,{
+                data,
+                expiry:Date.now()+duration
+            });
+            return originalJson(data);
+        };
+        next();
+    };
+
+};
+module.exports=cache;
--- tanisha/week1/day2-middleware/src/routes/productRoutes.js
+++ tanisha/week1/day2-middleware/src/routes/productRoutes.js
@@ -5,8 +5,9 @@ const validateProduct = require("../middleware/validateProduct.js");
 const validatePartialProduct=require("../middleware/validateProductPartial.js");
 const protect = require("../middleware/authMiddleware.js");
 const authorize=require("../middleware/authorize.js");
-router.get("/api/products",productController.getAllProducts);
-router.get("/api/products/:id",productController.getProductsById);
+const cache=require("../middleware/cache.js");
+router.get("/api/products",cache(60000),productController.getAllProducts);
+router.get("/api/products/:id",cache(60000),productController.getProductsById);
 //admin only
 router.post("/api/products",protect,authorize("user"),validateProduct,productController.createProducts);
 router.put("/api/products/:id",protect,authorize("user"),validateProduct,productController.updateProduct);

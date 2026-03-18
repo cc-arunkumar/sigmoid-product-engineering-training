b2b3b70869eafc20d11aefa191032dbc7a0b894c
nikhilkgupta2 <nikhilkumar@sigmoidanalytics.com>
2026-03-18 17:48:08 +0530

    Update app.js

--- day-3/app.js
+++ day-3/app.js
@@ -7,22 +7,22 @@ app.use(express.json());
 
 // ! adding middlewere
 const {errorHandler ,logger} = require("./middlewere/logger")
+
+const {apiLimiter} = require("./middlewere/rateLimiter")
 app.use(logger)
 
+app.use(apiLimiter)
 
 const productroute = require("./Routes/route")
+
+const authroutes = require("./Routes/authRoutes") ;
 app.use(productroute);
-
-
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
-
-
+app.use("/api/auth" , authroutes)
 app.use(errorHandler);
-
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");

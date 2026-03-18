4d778a9dc28b5d6940ecfcf9e9046207051034e7
siyakalra-work <siya.kalra@sigmoidanalytics.com>
2026-03-18 17:37:36 +0530

    feat(auth): implement role-based authorization middleware (RBAC

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,28 +2,40 @@ const jwt = require("jsonwebtoken")
 const { successResponse } = require("../utils/apiResponse");
 const AppError = require("../utils/AppError")
 
-const USER = {
-    id: 1,
-    username: "admin",
-    password: "1234"
-}
+const USER = [
+    {
+        id: 1,
+        username: "admin",
+        password: "1234",
+        role: "admin"
+    },
+    {
+        id: 2,
+        username: "user",
+        password: "1234",
+        role: "user"
+    }
+]
 
 exports.login = (req, res, next) => {
-    try {
+    try{
         const { username , password } = req.body;
         
         if(!username || !password){
             return next(new AppError("UserName and Password are requires", 400));
         }
 
-        if(USER.username !== username || USER.password !== password){
-            return next(new AppError("Invalid Credentials", 400))
+        const user = USER.find(u => u.username === username);
+
+        if(!user || user.password !== password){
+            return next(new AppError("Invalid Credentials", 401));
         }
 
         const token = jwt.sign(
             {
-                userId: USER.id,
-                username: USER.username
+                userId: user.id,
+                username: user.username,
+                role: user.role
             },
             process.env.JWT_SECRET || "mysecretkey",
             {   
@@ -33,7 +45,7 @@ exports.login = (req, res, next) => {
 
         return successResponse(res, "Login Succesfull", {token});
     } 
-    catch (error) {
+    catch(error){
         return next(new AppError(error.message || "Login Failed", 500));
     }
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,21 +5,22 @@ const productController = require("../controllers/productController");
 const validateProduct = require("../middleware/validateProduct");
 const validateProductPatch = require("../middleware/validateProductPatch");
 const protect=require("../middleware/authMiddleware");
+const authorize=require("../middleware/authorize")
 
 // GET
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id", productController.getProductById);
 
 // POST
-router.post("/api/products",protect, validateProduct, productController.createProduct);
+router.post("/api/products",protect,authorize("user"), validateProduct, productController.createProduct);
 
 // PUT
-router.put("/api/products/:id", validateProduct, productController.updateProduct);
+router.put("/api/products/:id",protect,authorize("user"), validateProduct, productController.updateProduct);
 
 // PATCH
-router.patch("/api/products/:id", validateProductPatch, productController.patchProduct);
+router.patch("/api/products/:id",protect,authorize("admin"), validateProductPatch, productController.patchProduct);
 
 // DELETE
-router.delete("/api/products/:id", productController.deleteProduct);
+router.delete("/api/products/:id",protect,authorize("admin"), productController.deleteProduct);
 
 module.exports = router;

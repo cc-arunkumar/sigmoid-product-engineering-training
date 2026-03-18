49f66ca03d9eddfe18b207fb58e8e1272814a131
Shubham <220108059shubham@gmail.com>
2026-03-18 16:23:56 +0530

    feat(auth): add JWT middleware and protect product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,13 +4,15 @@ const productController = require("../controllers/productController");
 
 const validateProduct = require("../middleware/validateProduct");
 
+const protect = require("../middleware/authMiddleware");
+
 // added /products
 router.get("/products", productController.getAllProducts);
 router.get("/product/:id", productController.getProductById);
 // create product
-router.post("/product", validateProduct , productController.createProduct);
-router.put("/product/:id",validateProduct, productController.updateProduct);
-router.delete("/product/:id" , productController.deleteProduct);
-router.patch("/product/:id", validateProduct ,productController.patchProduct);
+router.post("/product", protect ,validateProduct , productController.createProduct);
+router.put("/product/:id", protect ,validateProduct, productController.updateProduct);
+router.delete("/product/:id" , protect ,  productController.deleteProduct);
+router.patch("/product/:id", protect ,validateProduct ,productController.patchProduct);
 
 module.exports = router;

d663d11cdacf149ebd9c91b39ef39bf7beea81f9
siyakalra-work <siya.kalra@sigmoidanalytics.com>
2026-03-18 15:09:57 +0530

    feat(auth): implement JWT login and token generation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const app=express();
 const productRoutes = require("./routes/productRoutes");
+const authRoutes=require("./routes/authRoutes");
 const logger=require("./middleware/logger");
 const errorHandler=require("./middleware/errorHandler");
 app.use(express.json());
 app.use(logger);
-app.use("/",productRoutes);
+app.use("/api/product",productRoutes);
+app.use("./api/auth", authRoutes);
 app.use(errorHandler);
 app.listen(3000,()=>{
     console.log("Server running on port 3000")

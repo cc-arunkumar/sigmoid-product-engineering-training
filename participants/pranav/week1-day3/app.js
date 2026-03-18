bf5046f35dca57cbc1493a0cb2e02c9a540df5be
pranavky12-sigmoid <pranav.kumar@sigmoidanalytics.com>
2026-03-18 15:59:50 +0530

    feat(auth): implement JWT login and token generation

--- participants/pranav/week1-day3/app.js
+++ participants/pranav/week1-day3/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const authRouter = require("./routes/authRoutes");
+
+
  
 
 const app = express();
@@ -9,7 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(logger);
 
-
+app.use("/api/v1/",authRouter);
 app.use("/api/v1/products",productRoutes);
+
 app.use(errorHandler);
 module.exports = app;

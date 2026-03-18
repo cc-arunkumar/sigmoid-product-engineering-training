e1b5ffdb5c4676806fa608c8620782d52ad85f7b
Anish <anish01022@gmail.com>
2026-03-18 18:31:12 +0530

    feat(perfomance): add response caching middleware for GET endpoints

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,8 +5,9 @@ const validateProduct = require("../middlewares/validateProduct");
 const validateProductPatch = require("../middlewares/validateProductPatch");
 const protect = require("../middlewares/authMiddlewar");
 const authorize = require("../middlewares/authorize");
-router.get("/" , productController.getAllProducts);
-router.get("/:id", productController.getProductById);
+const cache = require("../middlewares/cache");
+router.get("/" ,cache(60000), productController.getAllProducts);
+router.get("/:id", cache(60000),productController.getProductById);
 router.post("/create" ,protect, authorize("user"),validateProduct, productController.createProduct);
 router.put("/:id" ,protect, authorize("user"), validateProduct, productController.updateProduct);
 router.delete('/delete/:id',protect, authorize("admin"), productController.deleteProduct)

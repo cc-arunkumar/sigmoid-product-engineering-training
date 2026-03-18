863534d80d75f7d2bb86150bc551f6d7c1a3add0
Aditya <aditya.kumar26@sigmoidanalytics.com>
2026-03-18 18:30:36 +0530

    feat(security): implement rate limiting for API and auth routes

--- week1-day3/app.js
+++ week1-day3/app.js
@@ -5,12 +5,13 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 app.use(express.json());
 
-
 const productRouter = require("./routes/productRoutes");
 const authRouter = require("./routes/authRoutes");
+const { apiLimiter } = require("./middleware/rateLimiter");
 
 app.use(logger);
-app.use("/api", productRouter)
+app.use(apiLimiter);
+app.use("/api", productRouter);
 app.use("/api/auth", authRouter);
 
 app.use(errorHandler);

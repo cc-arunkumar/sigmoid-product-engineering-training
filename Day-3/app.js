d470ac615e37b2b041e2a87cf4d05a5caf01491d
cs-ganesh <chanda.sai@sigmoidanalytics.com>
2026-03-18 15:24:31 +0530

    Update app.js

--- Day-3/app.js
+++ Day-3/app.js
@@ -3,15 +3,18 @@ const express=require("express")
 const app=express()
 
 const productRoutes=require("./routes/productRoutes");
+const authRoutes=require("./routes/authRoutes")
 
 const logger=require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+
 
 app.use(express.json())
 
 app.use(logger);
 
 app.use(productRoutes);
+app.use("/api/auth",authRoutes);
 
 app.use(errorHandler);
 

5dae8d4c30c27b6d9de18c224cc8559a92b7a12d
AS-MAC-1230 <as-mac-1230@AS-MAC-1230s-MacBook-Air.local>
2026-03-18 17:54:37 +0530

    feat(security): implement rate limiting for API and auth routes

--- templates/week1/day1-node-rest-api/app.js
+++ templates/week1/day1-node-rest-api/app.js
@@ -9,10 +9,12 @@ const orderRoutes = require("./routes/orderRoutes");
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const authRoutes = require("./routes/authRoutes");
+const { apiLimiter } = require("./middleware/rateLimiter");
 
 app.use(express.json()); //this is middlleware which helps use express json
 app.use(logger);
 
+app.use(apiLimiter);
 app.use(productRoutes);
 app.use(orderRoutes);
 app.use(userRoutes);
---
+++ templates/week1/day1-node-rest-api/middleware/rateLimiter.js
@@ -0,0 +1,33 @@
+const rateLimit = require("express-rate-limit");
+const AppError = require("../utils/AppError");
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  handler: (req, res, next) => {
+    return next(
+      new AppError("Too many requests, please try again later", 429)
+    );
+  }
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 3,
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  handler: (req, res, next) => {
+    return next(
+      new AppError("Too many login attempts. Try again later", 429)
+    );
+  }
+});
+
+module.exports = {
+  apiLimiter,
+  authLimiter
+};

e5f189afe71701a6e5ba9c1d7dee723fcd6aa341
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-18 18:25:25 +0530

    feat(performance): add response caching middleware for GET endpoints

--- templates/week1/day1-node-rest-api/src/routes/productRoutes.js
+++ templates/week1/day1-node-rest-api/src/routes/productRoutes.js
@@ -6,9 +6,10 @@ const protect = require("../middleware/authMiddleware")
 const authorize= require("../middleware/authorize")
 const validateProduct = require('../middleware/validateProduct');
 const validateProductPartial = require('../middleware/validateProductPartial');
+const cache=require("../middleware/cache");
 
-router.get("/api/products",productController.getAllProducts)
-router.get("/api/product/:id",productController.getProductById)
+router.get("/api/products",cache(60000),productController.getAllProducts)
+router.get("/api/product/:id",cache(60000),productController.getProductById)
 router.post("/api/products",protect,validateProduct,authorize("users"),productController.createProduct)
 router.put("/api/product/:id",validateProduct,protect,authorize("users"),productController.updateProduct)
 router.patch("/api/product/:id",validateProductPartial ,protect,authorize("admin"),productController.updateProductPartially)

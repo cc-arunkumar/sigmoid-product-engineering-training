05eb6e5a640bb20dfcea6341ce97e15a1e9b66db
AS-MAC-1154 <as-mac-1154@AS-MAC-1154s-MacBook-Air.local>
2026-03-18 15:56:22 +0530

    feat(auth): implement JWT login and token generation

--- tanisha/week1/day2-middleware/src/app.js
+++ tanisha/week1/day2-middleware/src/app.js
@@ -4,11 +4,14 @@ app.get("/api",(req,res)=>{
     res.send("Welcome to backend");
 })
 const productRoutes =require("./routes/productRoutes.js");
+const authRoutes = require("./routes/authRoutes.js");
+
 app.use(express.json());
 const logger=require("./middleware/logger.js");
 const errorHandler = require("./middleware/errorHandler.js");
 app.use(logger);
 app.use(productRoutes);
+app.use("/api", authRoutes);
 app.use(errorHandler);
 app.listen(3000,()=>{
     console.log("server running on port 3000");

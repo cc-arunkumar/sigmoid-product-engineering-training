932189708d1e2d1fac32cb2597bc9f3b0fd202b6
mohammadrustam-sigmoid <mohammad.rustam@sigmoidanalytics.com>
2026-03-18 17:24:25 +0530

    feat(auth): add JWT middleware and protect product routes

--- backend/week1/day3-node-rest-api/src/routes/authRoutes.js
+++ backend/week1/day3-node-rest-api/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { login } = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
 
 router.post("/login", login);
 
--- backend/week1/day3-node-rest-api/src/routes/productRoutes.js
+++ backend/week1/day3-node-rest-api/src/routes/productRoutes.js
@@ -5,12 +5,18 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const validateProduct = require("../middleware/validateProduct");
 const validatePatchProduct = require("../middleware/validatePatchProduct");
+const protect = require("../middleware/authMiddleware");
 
-router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getProductById);
-router.post("/", validateProduct, productController.createProduct);
-router.patch("/:id", validatePatchProduct, productController.patchProduct);
-router.put("/:id", validateProduct, productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+router.get("/", protect, productController.getAllProducts);
+router.get("/:id", protect, productController.getProductById);
+router.post("/", protect, validateProduct, productController.createProduct);
+router.patch(
+  "/:id",
+  protect,
+  validatePatchProduct,
+  productController.patchProduct,
+);
+router.put("/:id", protect, validateProduct, productController.updateProduct);
+router.delete("/:id", protect, productController.deleteProduct);
 
 module.exports = router;

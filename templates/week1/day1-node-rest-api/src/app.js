21994b7b751e7024c133f5b34e9a6870e7d6efdc
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-18 15:19:20 +0530

    feat(auth): implement JWT login and token generation

--- templates/week1/day1-node-rest-api/src/app.js
+++ templates/week1/day1-node-rest-api/src/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const productRoutes = require("./routes/productRoutes");
+const authRoutes=require("./routes/authRoutes")
 const logger=require("./middleware/logger")
 const errorHandler=require("./middleware/errorHandler")
 app.use(express.json());
+app.use(authRoutes)
 app.use(productRoutes);
 app.use(logger)
 app.use(errorHandler);

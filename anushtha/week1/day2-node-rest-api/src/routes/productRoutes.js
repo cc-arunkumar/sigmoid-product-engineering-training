98fb6751130ccb2eedc820599e44eceb321acffa
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-18 18:21:44 +0530

    feat(performance): add response caching middleware for GET endpoints

--- anushtha/week1/day2-node-rest-api/src/routes/productRoutes.js
+++ anushtha/week1/day2-node-rest-api/src/routes/productRoutes.js
@@ -4,9 +4,10 @@ const productController=require("../controllers/productController");
 const validateProduct=require("../middleware/validateProduct");
 const validatePartialProduct=require("../middleware/validatePartialProduct");
 const protect=require("../middleware/authMiddleware");
-const authorize=require("../middleware/authorize")
-router.get("/products",productController.getAllProducts);
-router.get("/products/:id",productController.getProductById);
+const authorize=require("../middleware/authorize");
+const cache=require("../middleware/cache.js");
+router.get("/products",cache(60000),productController.getAllProducts);
+router.get("/products/:id",cache(60000),productController.getProductById);
 router.post("/products",protect,authorize("user"),validateProduct,productController.createProduct);
 router.put("/products/:id",protect,authorize("user"),validateProduct,productController.updateProduct);
 router.delete("/products/:id",protect,authorize("admin"),productController.deleteProduct);

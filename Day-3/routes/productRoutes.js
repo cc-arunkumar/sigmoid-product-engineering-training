063823ab5c1f675c9a6328d70a68a7caee4355be
cs-ganesh <chanda.sai@sigmoidanalytics.com>
2026-03-19 11:46:51 +0530

    Update productRoutes.js

--- Day-3/routes/productRoutes.js
+++ Day-3/routes/productRoutes.js
@@ -5,12 +5,17 @@ const router=express.Router();
 const productController=require("../controllers/productController.js");
 const validateProduct = require("../middleware/validateProduct.js");
 const validateProductPartial=require("../middleware/validateProductPartial.js");
+const protect=require("../middleware/authMiddleware.js"); //
+const authorize=require("../middleware/authorize.js");  //
 
+//Public routes
 router.get("/api/products",productController.getAllProducts);
 router.get("/api/products/:id",productController.getProductById);
-router.post("/api/products", validateProduct ,productController.createProduct);
-router.put("/api/products/:id", validateProduct,productController.updateProduct); 
-router.delete("/api/products/:id",productController.deleteProduct);
-router.patch('/api/products/:id', validateProductPartial ,productController.patchProduct);
 
+//Admin only routes
+router.post("/api/products", protect, authorize("user"), validateProduct, productController.createProduct);
+router.put("/api/products/:id", protect, authorize("user"), validateProduct, productController.updateProduct); 
+router.delete("/api/products/:id", protect, authorize("admin"), productController.deleteProduct);
+// router.patch('/api/products/:id', validateProductPartial ,productController.patchProduct);
+router.patch('/api/products/:id', protect, authorize("admin"), validateProductPartial ,productController.patchProduct);
 module.exports=router;

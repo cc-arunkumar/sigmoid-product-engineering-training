622c70b721e9438fd0073be041be85458ea00801
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-19 10:38:51 +0530

    refactor(controller): integrate AppError for standardized error handling and integrated AppResponse for standardized responses

--- Participants/NikhilCS/week1-day2/src/middleware/validateProduct.js
+++ Participants/NikhilCS/week1-day2/src/middleware/validateProduct.js
@@ -1,28 +1,33 @@
+import { AppError } from "../utils/AppError";
 const validateProduct = (req, res, next) => {
   const {name,price,category,stock} = req.body;
   if (!name||name.trim()==="") {
-    return res.status(400).json({
-      success: false,
-      message: "Product name is required"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Product name is required"
+    // });
+    return next(new AppError("Product name is required", 400));
   }
   if (price ===undefined || price <= 0) {
-    return res.status(400).json({
-      success: false,
-      message: "invalid price value"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "invalid price value"
+    // });
+    return next(new AppError("Invlaid Price Value", 400));
   }
   if (!category || category.trim() === "") {
-    return res.status(400).json({
-      success: false,
-      message: "Category is required"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Category is required"
+    // });
+    return next(new AppError("Category is required", 400));
   }
   if (stock === undefined || stock < 0) {
-    return res.status(400).json({
-      success: false,
-      message: "invalid stock value"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "invalid stock value"
+    // });
+    return next(new AppError("Invalid Stock Value", 400))
   }
   
   next();
--- Participants/NikhilCS/week1-day2/src/middleware/validateProductPartial.js
+++ Participants/NikhilCS/week1-day2/src/middleware/validateProductPartial.js
@@ -1,34 +1,38 @@
 const validateProductPartial = (req, res, next) => {
   const { name, price, category, stock } = req.body;
-  
-  if (name!==undefined && name.trim() === "") {
-    return res.status(400).json({
-      success: false,
-      message: "Product name cannot be empty"
-    });
+
+  if (name !== undefined && name.trim() === "") {
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Product name cannot be empty"
+    // });
+    return next(new AppError("Product name cannto be empty", 400));
   }
-  
+
   if (price !== undefined && price <= 0) {
-    return res.status(400).json({
-      success: false,
-      message: "Price must be greater than 0"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Price must be greater than 0"
+    // });
+    return next(new AppError("Price must be greater than 0", 400));
   }
-  
-  if (category!==undefined && category.trim() === "") {
-    return res.status(400).json({
-      success: false,
-      message: "Category cannot be empty"
-    });
+
+  if (category !== undefined && category.trim() === "") {
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Category cannot be empty"
+    // });
+    return next(new AppError("Category cannot be empty", 400));
   }
-  
+
   if (stock !== undefined && stock < 0) {
-    return res.status(400).json({
-      success: false,
-      message: "Stock cannot be negative"
-    });
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Stock cannot be negative"
+    // });
+    return next(new AppError("Stock cannot be negative in value", 400));
   }
-  
+
   next();
 };
 

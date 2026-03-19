065489882f6a22f23007ce8a8795515d037f6faa
akhilgoyal-source <akhil.goyal@sigmoidanalytics.com>
2026-03-19 15:35:50 +0530

    feat(mongoDB): connected to mongoDB successfully & performed all CRUD operations

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-require("dotenv").config();
+require("dotenv").config()
 const express = require("express");
+
 const app = express();
 
-const productRoutes = require("./routes/productRoutes");
-const authRoutes = require("./routes/authRoutes");
 
-const logger = require("./middleware/logger");
-const errorHandler = require("./middleware/errorHandler");
-const { apiLimiter } = require("./middleware/rateLimiter");
+const productRoutes = require("./routes/productRoutes")
+const authRoutes = require("./routes/authRoutes")
 
+const logger = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
+const { apiLimiter } = require("./middleware/rateLimiter")
 const passport = require("./config/passport");
+const connectDB = require("./config/mongo");
 
-// ================= MIDDLEWARE =================
+connectDB();
+
 app.use(express.json());
 app.use(logger);
 app.use(apiLimiter);
+
 app.use(passport.initialize());
 
-// ================= ROUTES =================
+
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
-// ================= ERROR HANDLER =================
-app.use(errorHandler);
+app.use(errorHandler)
 
-// ================= SERVER =================
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get('/', (req, res) => { 
+  res.send("API Running"); 
+}); 
+
+console.log("ENV PORT:", process.env.PORT); 
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => { 
+  console.log(`Server running on port ${PORT}`); 
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,24 +4,27 @@ const router = express.Router();
 const productController = require("../controllers/productControllers");
 const validateProduct = require("../middleware/validateProduct");
 const validateProductPatch = require("../middleware/validateProductPatch");
-const protect =require("../middleware/authMiddleware");
+const protect=require("../middleware/authMiddleware");
 const authorize=require("../middleware/authorize");
 const cache=require("../middleware/cache");
-// GET
-router.get("/api/products", cache(6000),productController.getAllProducts);
-router.get("/api/products/:id", cache(6000),productController.getProductById);
+const productMongoControllers = require("../controllers/productMongoController")
 
+// GET
+router.get("/api/products",cache(60000), productController.getAllProducts);
+router.get("/api/products/:id",cache(60000), productController.getProductById);
 
 // POST
-router.post("/api/products",protect,authorize("user") ,validateProduct, productController.createProduct);
+router.post("/api/products",protect,authorize("user"), validateProduct, productController.createProduct);
 
 // PUT
-router.put("/api/products/:id",protect,authorize("user"),  validateProduct, productController.updateProduct);
+router.put("/api/products/:id",protect,authorize("user"), validateProduct, productController.updateProduct);
 
 // PATCH
-router.patch("/api/products/:id", protect,authorize("admin"),validateProductPatch, productController.patchProduct);
+router.patch("/api/products/:id",protect,authorize("admin"), validateProductPatch, productController.patchProduct);
 
 // DELETE
-router.delete("/api/products/:id", protect,authorize("admin"),productController.deleteProduct);
+router.delete("/api/products/:id",protect,authorize("admin"), productController.deleteProduct);
+
+router.post("/mongo", validateProduct, productMongoControllers.createProductMongo);
 
 module.exports = router;

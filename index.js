679a4c14e9feb4547d152a33b04fdef287b92e18
Utsav <utsav.kumar@sigmoidanalytics.com>
2026-03-19 13:00:38 +0530

    feat(auth): integrate Google OAuth2 with JWT token generation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require("express");
 
 const app = express();
+
 
 const productRoutes = require("./routes/productRoutes")
 const authRoutes = require("./routes/authRoutes")
@@ -8,11 +10,15 @@ const authRoutes = require("./routes/authRoutes")
 const logger = require("./middleware/logger")
 const errorHandler = require("./middleware/errorHandler")
 const { apiLimiter } = require("./middleware/rateLimiter")
+const passport = require("./config/passport");
+
 
 app.use(express.json());
 app.use(logger);
-
 app.use(apiLimiter);
+
+app.use(passport.initialize());
+
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,24 @@ const router = express.Router()
 
 const { login } = require("../controllers/authController");
 const { authLimiter } = require("../middleware/rateLimiter")
+const { googleCallback } = require("../controllers/authController");
+const passport = require("passport");
 
 
 router.post("/login", authLimiter, login);
 
+// Step 1: Redirect to Google
+router.get(
+    "/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
+
+// Step 2: Callback from Google
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { session: false }),
+    googleCallback
+);
 module.exports = router;

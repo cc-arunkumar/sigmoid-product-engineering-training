47f7699fe13ba0881eb00a821157d8fb073b4fa3
madhur6712 <madhur.suresh@sigmoidanalytics.com>
2026-03-20 18:58:21 +0530

    feat(mongo and MySQL): connected to mongoDB & MySQL hybrid Database connection established successfully & performed all CRUD operations

---
+++ participants/madhur-rao/week1-day5/ecommerce-app/config/sqlConnect.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// Create Sequelize instance
+const sequelize = new Sequelize(
+    process.env.SQL_DB,
+    process.env.SQL_USER,
+    process.env.SQL_PASSWORD,
+    {
+        host: process.env.SQL_HOST,
+        port: process.env.SQL_PORT,
+        dialect: "mysql",
+        logging: false
+    }
+);
+
+
+// Test connection
+const connectSQL = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("MySQL connected successfully");
+    } catch (error) {
+        console.error("MySQL connection failed:", error.message);
+        process.exit(1);
+    }
+};
+module.exports = { sequelize, connectSQL };
---
+++ participants/madhur-rao/week1-day5/ecommerce-app/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+
+
+const protect = (req, res, next) => {
+    try {
+        let token;
+        // 1. Extract token from Authorization header
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+        // 2. If no token
+        if (!token) {
+            return next(new AppError("Access denied. No token provided", 401));
+        }
+        // 3. Verify token
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET || "mysecretkey"
+        );
+        // 4. Attach user info to request
+        req.user = decoded;
+        next();
+    } catch (error) {
+        // Handle JWT specific errors
+        if (error.name === "JsonWebTokenError") {
+            return next(new AppError("Invalid token", 401));
+        }
+        if (error.name === "TokenExpiredError") {
+            return next(new AppError("Token expired", 401));
+        }
+        return next(new AppError("Authentication failed", 401));
+    }
+};
+module.exports = protect;

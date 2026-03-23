dcb14d4249616d543820edfb081b2970cfb1a364
Utsav <utsav.kumar@sigmoidanalytics.com>
2026-03-23 15:16:30 +0530

    Developed FastAPI REST Endpoint for getAll successfully

--- FastApi/ecommerce/app/controllers/product_controller.py
+++ FastApi/ecommerce/app/controllers/product_controller.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, APIRouter
+from app.services.product_service import get_all_products
 
 router = APIRouter(
     prefix="/api/products",
     tags=["Products"]
 )
+
+@router.get("/")
+def get_products():
+    products = get_all_products()
+    return products
 
 @router.get("/health")
 def system_health():
---
+++ FastApi/ecommerce/app/services/product_service.py
@@ -0,0 +1,20 @@
+
+products = [
+    {
+        "id": 1,
+        "name": "Laptop",
+        "price": 50000,
+        "category": "Electronics",
+        "stock": 25
+    },
+    {
+        "id": 2,
+        "name": "Mobile",
+        "price": 25000,
+        "category": "Electronics",
+        "stock": 100
+    }
+]
+
+def get_all_products():
+    return products

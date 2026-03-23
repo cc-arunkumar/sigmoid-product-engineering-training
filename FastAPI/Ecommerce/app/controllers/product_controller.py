7e58b1f5bce453f304226e237e79e39f6d511f64
Brxj19 <kumarbraj1925@gmail.com>
2026-03-23 16:45:10 +0530

    Developed FastAPI Endpoint for addProduct(post) successfully

--- FastAPI/Ecommerce/app/controllers/product_controller.py
+++ FastAPI/Ecommerce/app/controllers/product_controller.py
@@ -1,5 +1,13 @@
+"""
+    Product Controller: Handles API endpoints related to products.
+"""
+
+"""
+    IMPORTS
+"""
 from fastapi import APIRouter, HTTPException
-from app.services.product_service import get_all_products, get_product_by_id
+from app.services.product_service import get_all_products, get_product_by_id, create_product
+from app.models.product_model import Product
 
 product_router = APIRouter(
     prefix="/api/products",
@@ -16,3 +24,13 @@ def get_product(product_id: int):
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
     return product
+
+@product_router.post("/")
+def add_product(product: Product):
+    new_product = create_product(product)
+    return {
+        "status" : 201,
+        "success" : True,
+        "data" : new_product,
+        "message" : "Product created successfully"
+    }
--- FastAPI/Ecommerce/app/services/product_service.py
+++ FastAPI/Ecommerce/app/services/product_service.py
@@ -1,7 +1,7 @@
 products = [
-    {"id": 1, "name": "Laptop", "price": 999.99},
-    {"id": 2, "name": "Smartphone", "price": 499.99},
-    {"id": 3, "name": "Headphones", "price": 199.99},
+    {"id": 1, "name": "Laptop", "price": 999.99, "category": "Electronics","stock" : 10},
+    {"id": 2, "name": "Smartphone", "price": 499.99, "category": "Electronics","stock" : 20},
+    {"id": 3, "name": "Headphones", "price": 199.99, "category": "Electronics","stock" : 15},
 ]
 
 def get_all_products():
@@ -12,3 +12,8 @@ def get_product_by_id(product_id : int):
         if product["id"] == product_id:
             return product
     return None
+
+def create_product(product_data):
+    new_product = {"id" : len(products) + 1, **product_data.dict()}
+    products.append(new_product)
+    return new_product

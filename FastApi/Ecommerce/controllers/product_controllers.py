e5afad49120ffb9bdc4d141ec67308b2a0dc5a27
Anish <anish01022@gmail.com>
2026-03-23 17:34:17 +0530

    Developed FastAPI REST Endpoint for PUT successfully!

--- FastApi/Ecommerce/controllers/product_controllers.py
+++ FastApi/Ecommerce/controllers/product_controllers.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI  , APIRouter , HTTPException
-from services.product_service import get_all_products , get_product_by_id , create_product
+from fastapi import APIRouter , HTTPException
+from services.product_service import get_all_products , get_product_by_id , create_product , update_product
 from models.products_model import Product
 
 router  = APIRouter(
@@ -9,8 +9,8 @@ router  = APIRouter(
 
 @router.get("/")
 def get_products():
-    prodcuts = get_all_products()
-    return prodcuts
+    products = get_all_products()
+    return products
 
 @router.get("/health")
 def system_health():
@@ -28,5 +28,14 @@ def get_product_id(product_id : int):
 @router.post("/")
 def add_product(product_details :Product):
     product = create_product(product_details = product_details)
+    
+    if not product:
+        raise HTTPException(status_code = 400 , detail = "Failed to create product !")
+    
+    return product
+
+@router.put("/{product_id}")
+def modify_product(product_details : Product, product_id : int):
+    product = update_product(product_details=product_details, product_id=product_id)
     return product
   
--- FastApi/Ecommerce/services/product_service.py
+++ FastApi/Ecommerce/services/product_service.py
@@ -16,10 +16,10 @@ products = [
 ]
 
 
-def get_all_products():
+def get_all_products() -> list:
     return products
 
-def get_product_by_id(product_id : int):
+def get_product_by_id(product_id : int) -> dict:
     for product in products : 
         if product["id"] == product_id :
             return product
@@ -27,10 +27,26 @@ def get_product_by_id(product_id : int):
     return None
 
 
-def create_product(product_details):
+def create_product(product_details) -> dict:
     product = product_details.dict()
     product["id"] = len(products) + 1
     products.append(product)
     return product
 
+
+# PUT to update product
+def update_product(product_details, product_id):
+    new_product = product_details.dict()
+    new_product["id"] = product_id
+    updated = False
+    for i in range(0, len(products)):
+        if products[i]["id"] == product_id:
+            products[i] = new_product
+            updated = True
+            break
+    
+    if updated:
+        return {"Updated Successfully"}
+    else:
+        return {"Product not found"}
 

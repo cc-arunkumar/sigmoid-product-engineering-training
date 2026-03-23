ff3c4c6f58fb0de2d8f249341086028abe35f596
Utkarsh Kumar <utkarsh.kumar@sigmoidanalytics.com>
2026-03-23 15:07:51 +0530

    developed rest api for getting products using fastApi

---
+++ FastApi/Ecommerce/app/services/product_service.py
@@ -0,0 +1,25 @@
+products=[
+    {
+        "id": 1,
+        "name": "Product 1",
+        "price": 10.99,
+        "category": "Category A",
+        "stock": 50
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "price": 19.99,
+        "category": "Category B",
+        "stock": 30
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "price": 5.99,
+        "category": "Category A",
+        "stock": 100
+    }
+]
+def get_all_products(): 
+    return products

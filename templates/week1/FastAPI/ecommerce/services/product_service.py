2a969a5b5d27096fd6db9188ba0f27c6bf415da3
ankit17082 <ankitkumar@sigmoidanalytics.com>
2026-03-23 15:39:09 +0530

    Developed FastAPI REST Endpoint for getById successfully!

--- templates/week1/FastAPI/ecommerce/services/product_service.py
+++ templates/week1/FastAPI/ecommerce/services/product_service.py
@@ -15,5 +15,13 @@ products = [
     }
 ]
 
+# GET All Products
 def get_all_products():
     return products
+
+# GET Products By Id
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            return product
+    return None

1fec8c25f98308e67332705bb771493520b9dd7d
Om Jadhav <om.vasant@sigmoidanalytics.com>
2026-03-23 15:08:32 +0530

    Developed FastAPI REST Endpoint

--- FastAPI/e-commerce/app/controllers/product_controller.py
+++ FastAPI/e-commerce/app/controllers/product_controller.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
+from app.services.product_service import get_all_products
 
 router = APIRouter(
     prefix="/api/product",
     tags=["Products"]
 )
 
-@router.get("/health")
-def health_check():
-    return {"status": "Product APIs running smoothly!"}
+# @router.get("/health")
+# def health_check():
+#     return {"status": "Product APIs running smoothly!"}
+
+@router.get("/")
+def get_products():
+    products = get_all_products()
+    return products
+
---
+++ FastAPI/e-commerce/app/services/product_service.py
@@ -0,0 +1,19 @@
+products = [
+    {
+        "id": 1,
+        "name": "Laptop",
+        "price": 100000,
+        "category": "Electronics",
+        "stock": 10
+    },
+    {
+        "id": 2,
+        "name": "Mobile",
+        "price": 50000,
+        "category": "Electronics",
+        "stock": 15
+    }
+]
+
+def get_all_products():
+    return products

097d74c1cd3949d50f3af6e89cf7890ac61a3a3b
Brxj19 <kumarbraj1925@gmail.com>
2026-03-23 15:39:31 +0530

    Developed FastAPI Endpoint for getById successfully

--- FastAPI/Ecommerce/app/controllers/product_controller.py
+++ FastAPI/Ecommerce/app/controllers/product_controller.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from app.services.product_service import get_all_products
+from fastapi import APIRouter, HTTPException
+from app.services.product_service import get_all_products, get_product_by_id
 
 product_router = APIRouter(
     prefix="/api/products",
@@ -9,3 +9,10 @@ product_router = APIRouter(
 @product_router.get("/")
 def get_products():
     return get_all_products()
+
+@product_router.get("/{product_id}")
+def get_product(product_id: int):
+    product = get_product_by_id(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product

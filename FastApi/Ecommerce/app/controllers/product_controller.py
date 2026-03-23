9ef10a761c9cdd75f98697a57c239c35763c47d7
akhilgoyal-source <akhil.goyal@sigmoidanalytics.com>
2026-03-23 16:21:48 +0530

    FastAPI REST Endpoint for POST successfully!

--- FastApi/Ecommerce/app/controllers/product_controller.py
+++ FastApi/Ecommerce/app/controllers/product_controller.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from app.services.product_service import get_all_products
 from app.services.product_service import get_all_products, get_product_by_id
+from app.services.product_service import create_product
+from app.models.product_model import Product
 router=APIRouter(
     prefix="/api/product",
     tags=["Products"]
@@ -17,3 +19,9 @@ def get_product_id(product_id:int):
     if not product:
         raise HTTPException(status_code=404,detail="Product not found!")
     return product
+
+@router.post("/")
+def add_product(product: Product):
+    return create_product(product)
+
+

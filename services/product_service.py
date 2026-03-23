52526b1b6a1267d9d43c0832b5bd73c292909d9d
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-23 15:13:51 +0530

    Developed FastAPI REST Endpoint for getAll successfully!

---
+++ services/product_service.py
@@ -0,0 +1,19 @@
+products = [
+    {
+        "id": 1,
+        "name": "Product 1",
+        "price": 100,
+        "category": "Category A",
+        "stock": 10
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "price": 200,
+        "category": "Category B",
+        "stock": 20
+    }
+]
+
+def get_all_products():
+    return products

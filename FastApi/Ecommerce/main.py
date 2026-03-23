e72241450e17e09544421bcfa695c4f004a335b9
akhilgoyal-source <akhil.goyal@sigmoidanalytics.com>
2026-03-23 14:58:40 +0530

    fast api initiated using swagger docs

---
+++ FastApi/Ecommerce/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import app.controllers.product_controller as product_router
+
+app=FastAPI()
+app.include_router(product_router.router)
+
+@app.get("/")
+def home():
+    return{"message":"FastAPI server is swimming"}

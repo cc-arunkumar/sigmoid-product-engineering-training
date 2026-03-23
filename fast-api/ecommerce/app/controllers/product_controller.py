0f0f69e5e84af43192ebf67802e7a92d6af3614e
AS-MAC-1154 <as-mac-1154@AS-MAC-1154s-MacBook-Air.local>
2026-03-23 16:24:42 +0530

    Developed FastAPI REST Endpoint for POST successfully

--- fast-api/ecommerce/app/controllers/product_controller.py
+++ fast-api/ecommerce/app/controllers/product_controller.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, HTTPException
 
 from app.services.product_service import get_all_products
 from app.services.product_service import get_product_by_id
-
+ 
+from app.services.product_service import create_product
+from app.models.product_model import Product   
 router = APIRouter(
     prefix="/api/products",
     tags=["products"]
@@ -24,3 +26,7 @@ def get_product(product_id: int):
     if not product:
         return HTTPException(status_code=404, detail="Product not found")
     return product
+@router.post("/")
+def add_product(product: Product):
+    new_product = create_product(product)
+    return new_product

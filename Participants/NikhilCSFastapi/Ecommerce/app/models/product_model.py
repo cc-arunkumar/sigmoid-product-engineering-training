f09546ac0a9f7eda4f1509b3de1746679ef3edea
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-23 18:42:26 +0530

    feat(validation):pydantic field validations implemented successfully

--- Participants/NikhilCSFastapi/Ecommerce/app/models/product_model.py
+++ Participants/NikhilCSFastapi/Ecommerce/app/models/product_model.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Optional
 # THIS MODEL IS A STRUCTURE TO FOLLOW FOR THE REQUEST BODY ALONE 
+# better to have seperate model sone for put+post wher everything is requiresd and one for patch similar to nodejs 
 class Product(BaseModel):
-    name:Optional[str]=None
-    price:Optional[int]=None
+    name:Optional[str]=None,Field(min_length=3,max_length=20)
+    price:Optional[int]=None,Field(lt=90000,gt=800)
     category:str=None
-    stock:int=None
+    stock:int=None,Field(gt=3)

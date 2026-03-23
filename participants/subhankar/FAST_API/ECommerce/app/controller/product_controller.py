91d5553c270eb9ff273ebc4bfea9a34bc1252b64
AS-MAC-1116 <as-mac-1116@AS-MAC-1116.local>
2026-03-23 15:14:50 +0530

    Developed FastAPT REST Endpoint for get All sucessfully

--- participants/subhankar/FAST_API/ECommerce/app/controller/product_controller.py
+++ participants/subhankar/FAST_API/ECommerce/app/controller/product_controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import app.services.product_service as product_service
 
 router=APIRouter(
     prefix="/api/product",
@@ -8,3 +9,9 @@ router=APIRouter(
 @router.get("/health")
 def health_check():
     return{"Message":"Product API is healthy !"}
+
+
+@router.get("/")
+def get_all_products():
+    products=product_service.get_all_products()
+    return{"Products":products}

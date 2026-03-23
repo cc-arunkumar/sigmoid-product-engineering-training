5dbed2a25bd06b88307360679686364b80c8aee7
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-23 15:14:27 +0530

    Developed FastAPI REST Endpoint for getAll successfully

--- Participants/NikhilCSFastapi/Ecommerce/app/controllers/product_controller.py
+++ Participants/NikhilCSFastapi/Ecommerce/app/controllers/product_controller.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
+from app.services.product_service import get_all_products
 router= APIRouter(
     prefix="/api/products",
     tags=["Products"]
 )
-@router.get("/health")
-def health_check():
-    return {"status":"Product APIs running !"}
+# @router.get("/health")
+# def health_check():
+#     return {"status":"Product APIs running !"}
+
+@router.get("/")
+def get_products():
+    products=get_all_products()
+    return products

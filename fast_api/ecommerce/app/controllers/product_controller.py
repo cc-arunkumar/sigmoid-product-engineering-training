fb5695c6b97f58a888f3fa588ae71902a05bc281
AS-MAC-1129 <as-mac-1129@AS-MAC-1129.local>
2026-03-23 15:29:27 +0530

    developed fast api rest endpoint for get_products_by_id successfully

--- fast_api/ecommerce/app/controllers/product_controller.py
+++ fast_api/ecommerce/app/controllers/product_controller.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from app.services.product_service import get_all_products
+from app.services.product_service import get_product_by_id
 
 router = APIRouter(
     prefix="/api/products",
@@ -12,9 +13,14 @@ def get_products():
     products=get_all_products()
     return products
 
+@router.get("/{product_id}")
+def get_product(product_id: int):
+    product = get_product_by_id(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
 
 
-    
 
 # @router.get("/health")
 # def health_check():

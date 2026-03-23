67f90f22cc15bed9e0d39d2549a7900c54dcb021
AS-MAC-1116 <as-mac-1116@AS-MAC-1116.local>
2026-03-23 17:50:04 +0530

    Developed FastAPI REST Endpoint for post, put and delete successfully!

--- participants/subhankar/FAST_API/ECommerce/app/controller/product_controller.py
+++ participants/subhankar/FAST_API/ECommerce/app/controller/product_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
-from app.services.product_service import get_all_products, get_product_by_id
+from app.services.product_model import Product
+from app.services.product_service import delete_product, get_all_products, get_product_by_id, create_product, update_product, delete_product
 
 
 router=APIRouter(
@@ -13,7 +14,7 @@ def health_check():
 
 
 @router.get("/")
-def get_all_products():
+def get_products():
     products=get_all_products()
     return{"Products":products}
 
@@ -24,3 +25,18 @@ def get_product_id(product_id:int):
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
     return product
+
+@router.post("/")
+def add_product(product:Product):
+    return create_product(product)
+
+@router.put("/{product_id}")
+def update_product_(product_id:int, updated_product:Product):
+    return update_product(product_id, updated_product)
+
+@router.delete("/{product_id}")
+def remove_product_(product_id:int):
+    if delete_product(product_id):
+        return{"Message":"Product deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Product not found")
--- participants/subhankar/FAST_API/ECommerce/app/services/product_service.py
+++ participants/subhankar/FAST_API/ECommerce/app/services/product_service.py
@@ -14,11 +14,39 @@ products = [
     "stock":15
 }
 ]
+
+# CRUD Operations
+
+# Read All
 def get_all_products():
     return products
 
+# Read by id
 def get_product_by_id(product_id:int):
     for product in products:
         if product["id"]==product_id:
             return product
     return None
+
+# Create
+def create_product(product_data):
+    new_product=product_data.dict()
+    new_product["id"]=len(products)+1
+    products.append(new_product)
+    return new_product
+
+# Update
+def update_product(product_id:int, updated_data):
+    for product in products:
+        if product["id"]==product_id:
+            product.update(updated_data.dict())
+            return product
+    return None
+
+# Delete
+def delete_product(product_id:int):
+    for product in products:
+        if product["id"]==product_id:
+            products.remove(product)
+            return True
+    return False

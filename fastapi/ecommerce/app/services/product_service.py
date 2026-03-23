d72870134975cb63bfb636f03dc42b94ccd4d66d
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-23 15:09:47 +0530

    Developed FastAPI REST Endpoint for getAll successfully

---
+++ fastapi/ecommerce/app/services/product_service.py
@@ -0,0 +1,25 @@
+products=[
+    {
+      "id":101,
+      "name":"Laptop",
+      "price":100000.0,  
+      "category":"Electronics",
+      "stock":20
+    },
+    {
+      "id":102,
+      "name":"Mobile",
+      "price":25000.0,  
+      "category":"Electronics",
+      "stock":45
+    },
+    {
+      "id":103,
+      "name":"Headphones",
+      "price":25000.0,  
+      "category":"Electronics",
+      "stock":50      
+    }
+  ]
+def get_all_products():
+    return products

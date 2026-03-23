b15be4aaec504fd8884e82f767b221d9575463d6
akhilgoyal-source <akhil.goyal@sigmoidanalytics.com>
2026-03-23 15:41:09 +0530

    FAst Api end point by getid

--- FastApi/Ecommerce/app/controllers/product_controller.py
+++ FastApi/Ecommerce/app/controllers/product_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.services.product_service import get_all_products
+from app.services.product_service import get_all_products, get_product_by_id
 router=APIRouter(
     prefix="/api/product",
     tags=["Products"]
@@ -9,3 +10,10 @@ router=APIRouter(
 def get_products():
     products=get_all_products()
     return products
+@router.get("/{product_id}")
+def get_product_id(product_id:int):
+    product=get_product_by_id(product_id)
+
+    if not product:
+        raise HTTPException(status_code=404,detail="Product not found!")
+    return product

abf182dd13e4d8957102da0cba89fd638ef63955
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-23 18:43:13 +0530

    Pydantic Field validation implemented successfully

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Product(BaseModel):
     id: int | None = None
-    name: str
-    price: float
+    name: str = Field(min_length=3, max_length=50)
+    price: int = Field(gt=0, lt=99999)
     category: str
     stock: int

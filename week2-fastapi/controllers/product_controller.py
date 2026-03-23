9c7a2895e877a0fdcb4a1540936fb584dec0bbe1
PrakharMishra-1443 <prakhar.mishra@sigmoidanalytics.com>
2026-03-23 15:09:47 +0530

    feat(controller) GET /api/product endpoint added successfully

--- week2-fastapi/controllers/product_controller.py
+++ week2-fastapi/controllers/product_controller.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-
+from services.product_service import get_all_products
 router = APIRouter(
     prefix="/api/product",
     tags=["Products"]  
 )
-
+@router.get("/")
+def get_products():
+    return get_all_products()
 @router.get("/health")
 def health_check():
     return {"status": "Product APIs running!"}

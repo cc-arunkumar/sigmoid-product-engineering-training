a5d26f3f95c69b107006958bf03b33f38e99205b
AS-MAC-1196 <as-mac-1196@AS-MAC-1196s-MacBook-Air.local>
2026-03-23 15:38:29 +0530

    Developed FastAPI REST Endpoint for getByID successfully

--- fastapi/ecommerce/app/services/product_service.py
+++ fastapi/ecommerce/app/services/product_service.py
@@ -21,5 +21,12 @@ products=[
       "stock":50      
     }
   ]
+# Get all products
 def get_all_products():
     return products
+# Get product by ID
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            return product
+    return None

2018faa6570b52b0cc29935f3f19d7ae43ff44d7
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-23 17:52:09 +0530

    feat(controller): POST /api/products and PUT /api/products endpoint added successfully

--- week2-fastApi/app/controllers/productController.py
+++ week2-fastApi/app/controllers/productController.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.services.productService import getAllProducts
+from fastapi import HTTPException
+from app.services.productService import getAllProducts,get_product_by_id,create_product,update_product,delete_product
+from app.models.product_model import Product
 router=APIRouter(
     prefix="/api/products",
     tags=["Products"]
@@ -8,3 +10,38 @@ router=APIRouter(
 def getProducts():
     return getAllProducts()
     
+@router.get("/{product_id}")
+def get_product_id(product_id: int):
+    product = get_product_by_id(product_id)
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found !")
+
+    return product    
+
+@router.post("/")
+def add_product(product: Product):
+    return create_product(product)
+
+
+@router.put("/{product_id}")
+def update_product_api(product_id: int, product: Product):
+    updated_product = update_product(product_id, product)
+
+    if not updated_product:
+        raise HTTPException(status_code=404, detail="Product not found!")
+
+    return updated_product
+
+
+@router.delete("/{product_id}", status_code=200)
+def delete_product_api(product_id: int):
+    deleted_product = delete_product(product_id)
+
+    if not deleted_product:
+        raise HTTPException(status_code=404, detail="Product not found!")
+
+    return {
+        "message": "Product deleted successfully",
+        "product": deleted_product
+    }
--- week2-fastApi/app/services/productService.py
+++ week2-fastApi/app/services/productService.py
@@ -13,11 +13,59 @@ products = [
         "stock": 80
     }, {
         "id": 3,
-        "name": "Headphones",
-        "price": 3000,
+        "name": "oneplus_headphone",
+        "price": 300,
         "category": "Electronics",
         "stock": 150
+    },
+    {
+        "id": 4,
+        "name": "samsung_galaxy",
+        "price": 30000,
+        "category": "Electronics",
+        "stock": 15000
     }
 ]
 def getAllProducts():
     return products
+
+    # GET Products By Id
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            return product
+    return None
+
+# POST Product
+
+def create_product(product_data):
+    new_product = product_data.dict()
+
+    # Generating ID manually
+    new_product["id"] = len(products) + 1
+
+    products.append(new_product)
+
+    return new_product
+
+# PUT PRODUCT
+def update_product(product_id, product_data):
+    for product in products:
+        if product["id"] == product_id:
+            updated_data = product_data.dict()   # or model_dump() (Pydantic v2)
+
+            product.update(updated_data)
+            return product
+
+    return None
+
+
+#DELETE PRODUCT
+
+def delete_product(product_id:int):
+   for index, product in enumerate(products):
+      if product["id"] == product_id:
+         deleted_product = products.pop(index)
+         return deleted_product 
+
+   return None

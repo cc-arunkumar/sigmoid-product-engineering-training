9f4fefb6d84ae04d392b1b88433455e21a1ab799
Abhinay-sig <abhinay.mangaldas@sigmoidanalytics.com>
2026-03-23 18:44:14 +0530

    Pydantic Field validations implemented successfully

--- FastAPI/ecommerce/app/models/product_model.py
+++ FastAPI/ecommerce/app/models/product_model.py
@@ -1,15 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , Field
 from typing import Optional
 
-class ProductIn(BaseModel):
-    name: str
-    price: int
-    category: str
-    stock: int
+# class ProductIn(BaseModel):
+#     name: str
+#     price: int
+#     category: str
+#     stock: int
+
+# class Product(BaseModel):
+#     name: Optional[str] = None
+#     price: Optional[int] = None
+#     category: Optional[str] = None
+#     stock: Optional[int] = None
 
 class Product(BaseModel):
-    name: Optional[str] = None
-    price: Optional[int] = None
-    category: Optional[str] = None
-    stock: Optional[int] = None
+    name: Optional[str] = Field(min_length=3, max_length=50)
+    price: int = Field(gt=0, lt=999999)
+    category: str = None
+    stock: int = None
     

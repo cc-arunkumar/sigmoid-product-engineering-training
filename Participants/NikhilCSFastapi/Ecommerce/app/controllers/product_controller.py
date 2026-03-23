8528b642d8f21a342ce61e6529ea87c5fb9eea90
AS-MAC-1285 <as-mac-1285@AS-MAC-1285.local>
2026-03-23 15:01:00 +0530

    fast api using uvicorn initiated and verfified with Swagger Docs and Postman successfully

---
+++ Participants/NikhilCSFastapi/Ecommerce/app/controllers/product_controller.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+router= APIRouter(
+    prefix="/api/products",
+    tags=["Products"]
+)
+@router.get("/health")
+def health_check():
+    return {"status":"Product APIs running !"}
--- Participants/NikhilCSFastapi/Ecommerce/main.py
+++ Participants/NikhilCSFastapi/Ecommerce/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from app.controllers.product_controller import router as product_router
 app=FastAPI()
+app.include_router(product_router)
 @app.get("/")
 def home():
     return {"message":"FastAPI server is running "}

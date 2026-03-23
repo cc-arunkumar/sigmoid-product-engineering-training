ede2adc214f53df76a382c68cbc4daa116911345
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-23 18:48:11 +0530

    Adding pydantic Field validation successfully

--- FAST-API/Ecommerce/app/models/product_model.py
+++ FAST-API/Ecommerce/app/models/product_model.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional 
+
+##adding pydantic Field validation
 
 class Product(BaseModel):
-    name : str
-    price : int
+    name : str = Field(min_length=3 , max_length=20)
+    price : int = Field(gt = 0 , lt = 99999)
     category : str
-    stock : int
+    stock : int = Field(gt = -1)
 
 
 class ProductPartial(BaseModel):
-    name : Optional[str] = None
-    price : Optional[int] = None
+    name : Optional[str] = Field(default=None, min_length=3 , max_length=20)
+    price : Optional[int] = Field(default=None,gt = 0 , lt = 99999)
     category :Optional[str] = None
-    stock : Optional[int] = None
+    stock : Optional[int] = Field(default=None,gt = -1)
+

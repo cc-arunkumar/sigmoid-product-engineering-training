07fcfb70f63c10c497772d856b078fcf9229da33
AS-MAC-1270 <as-mac-1270@AS-MAC-1270s-MacBook-Air.local>
2026-03-23 15:43:00 +0530

    Developed FastAPI REST Endpoint for getByID successfully

--- templates/week1/day1-node-rest-api/participants/abhinendra/FAST_API/ECOMMERCE_FAST_API/app/controllers/producrt_controller.py
+++ templates/week1/day1-node-rest-api/participants/abhinendra/FAST_API/ECOMMERCE_FAST_API/app/controllers/producrt_controller.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.services.product_service import get_all_products,get_product_by_id
 
-from app.services.product_service import get_all_products
+
 router = APIRouter(
     prefix="/api/products",
     tags=["products"]
@@ -9,3 +10,15 @@ router = APIRouter(
 @router.get("/")
 def get_products():
     return get_all_products()
+
+
+
+#GET products by ID
+@router.get("/{product_id}")
+def get_product(product_id: int):
+    product = get_product_by_id(product_id)
+
+    if not product:
+        raise HTTPException(status_code= 404, detail= "Product not Found")
+
+    return product

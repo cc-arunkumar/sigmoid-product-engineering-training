c903b45582fa8a7e63fd2d4b96eade1218a188fe
Utkarsh Kumar <utkarsh.kumar@sigmoidanalytics.com>
2026-03-23 18:41:39 +0530

    pydantic validation done successfully

--- FastApi/Ecommerce/app/models/products_model.py
+++ FastApi/Ecommerce/app/models/products_model.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import  Optional
 class Product(BaseModel):
-    id: Optional[int] = None
-    name: Optional[str] = None
-    price: Optional[float] = None
-    category: Optional[str] = None
-    stock: Optional[int] = None
+    name: str = Field(min_length=3,max_length=50, example="Product 1")
+    price: float = Field(gt=0,lt=9999)
+    category: Optional[str] = Field(None, example="Category A")
+    stock: Optional[int] = Field(None, example=50)
--- FastApi/Ecommerce/app/services/product_service.py
+++ FastApi/Ecommerce/app/services/product_service.py
@@ -3,21 +3,21 @@ products=[
     {
         "id": 1,
         "name": "Product 1",
-        "price": 10.99,
+        "price": 10,
         "category": "Category A",
         "stock": 50
     },
     {
         "id": 2,
         "name": "Product 2",
-        "price": 19.99,
+        "price": 19,
         "category": "Category B",
         "stock": 30
     },
     {
         "id": 3,
         "name": "Product 3",
-        "price": 5.99,
+        "price": 5,
         "category": "Category A",
         "stock": 100
     }

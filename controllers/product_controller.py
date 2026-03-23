92e2ea41fbcce9ed01df41704b02eae4a6fc2559
saurabhkumar-sigmoid <saurabh.kumar@sigmoidanalytics.com>
2026-03-23 15:41:00 +0530

    Developed FastAPI REST Endpoint for getById successfully!

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from services.product_service import get_all_products
+from fastapi import APIRouter, HTTPException
+from services.product_service import get_all_products, get_product_by_id
 
 router = APIRouter(
     prefix="/api/products",
@@ -14,4 +14,11 @@ def get_products():
 @router.get("/health")
 def health_check():
     return {"status": "Product API is running"}
+
+@router.get("/{product_id}")
+def get_product_ID(product_id: int):
+    product = get_product_by_id(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
 

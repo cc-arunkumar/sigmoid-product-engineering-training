0754e7ab1b8648dd9b39ec4ac2feb129adf578a7
Brxj19 <kumarbraj1925@gmail.com>
2026-03-23 19:01:44 +0530

    Added Field validation in the product schema

--- FastAPI/Ecommerce/app/models/product_model.py
+++ FastAPI/Ecommerce/app/models/product_model.py
@@ -1,14 +1,14 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Product(BaseModel):
-    name: str
-    price: float
-    category: str
-    stock: int
+    name: str = Field(...,min_length=1,max_length=100)
+    price: float = Field(..., gt=0)
+    category: str = Field(..., min_length=1, max_length=100)
+    stock: int = Field(..., ge=0)
 
 class ProductPatch(BaseModel):
-    name: Optional[str] = None
-    price: Optional[float] = None
-    category: Optional[str] = None
-    stock: Optional[int] = None
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    price: Optional[float] = Field(None, gt=0)
+    category: Optional[str] = Field(None, min_length=1, max_length=100)
+    stock: Optional[int] = Field(None, ge=0)

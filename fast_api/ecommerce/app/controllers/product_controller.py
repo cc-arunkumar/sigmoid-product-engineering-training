e1088538d0cfc14404cd73bc737b8e1a47198e8b
AS-MAC-1129 <as-mac-1129@AS-MAC-1129.local>
2026-03-23 16:09:45 +0530

    developed fast api rest endpoint for create products

--- fast_api/ecommerce/app/controllers/product_controller.py
+++ fast_api/ecommerce/app/controllers/product_controller.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter,HTTPException
 from app.services.product_service import get_all_products
-from app.services.product_service import get_product_by_id
+from app.services.product_service import get_product_by_id,create_product
+from app.models.product_model import Product
 
 router = APIRouter(
     prefix="/api/products",
     tags=["Products"]
 )
 
-
+#GET PRODUCTS
 @router.get("/")
 def get_products():
     products=get_all_products()
     return products
 
+#GET PRODUCTS BY ID
 @router.get("/{product_id}")
 def get_product(product_id: int):
     product = get_product_by_id(product_id)
@@ -20,6 +22,15 @@ def get_product(product_id: int):
         raise HTTPException(status_code=404, detail="Product not found")
     return product
 
+#CREATE PRODUCTS
+@router.post("/")
+def create(product_data:Product):
+    return create_product(product_data)
+
+# #DELETE PRODUCTS
+# @router.delete("/{product_id}")
+# def delete(product_id):
+    
 
 
 # @router.get("/health")
--- fast_api/ecommerce/app/services/product_service.py
+++ fast_api/ecommerce/app/services/product_service.py
@@ -21,11 +21,24 @@ products=[{
 }
 ]
 
+#GET ALL PRODUCTS
 def get_all_products():
     return products
 
+
+#GET PRODUCTS BY ID
 def get_product_by_id(product_id:int):
     for product in products:
         if product["id"] == product_id:
             return product
     return None
+
+#CREATE PRODUCTS
+def create_product(product_data):
+    newprod=product_data.dict()
+    newid=len(products)+1
+    newprod["id"]=newid
+    products.append(newprod)
+    return products
+    
+#DELETE PRODUCTS

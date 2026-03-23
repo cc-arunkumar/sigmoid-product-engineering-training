aa26c60989c241775b56c540dc37d0c16be42d71
AS-MAC-1321 <as-mac-1321@AS-MAC-1321s-MacBook-Air.local>
2026-03-23 15:13:25 +0530

    Developed fast api rest endpoint for get_all successfully

---
+++ FAST-API/Ecommerce/app/services/product_service.py
@@ -0,0 +1,19 @@
+products = [
+    {
+        "id" : 1,
+        "name" : "computer",
+        "price" : 250000,
+        "category": "electronics",
+        "stock" : 10
+    },
+    {
+        "id" : 2,
+        "name" : "laptop",
+        "price" : 50000,
+        "category": "electronics",
+        "stock" : 20
+    }
+]
+
+def get_all_products():
+    return products

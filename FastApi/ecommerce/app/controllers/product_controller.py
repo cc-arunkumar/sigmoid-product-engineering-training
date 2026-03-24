361841e19695479e6f9ba54cab48a33544271788
Utsav <utsav.kumar@sigmoidanalytics.com>
2026-03-24 12:58:54 +0530

    Implemented Database integration & performed end to end API testing for all CRUD operations

--- FastApi/ecommerce/app/controllers/product_controller.py
+++ FastApi/ecommerce/app/controllers/product_controller.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, APIRouter
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from app.services.product_service import get_all_products, get_product_by_id, create_product, update_product, delete_product, patch_update
-from app.models.products_model import Product
+from app.models.products_model import Product, ProductPatch
+from app.core.config import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix="/api/products",
@@ -9,13 +11,13 @@ router = APIRouter(
 )
 
 @router.get("/")
-def get_products():
-    products = get_all_products()
+def get_products(db: Session = Depends(get_db)):
+    products = get_all_products(db)
     return products
 
 @router.get("/{product_id}")
-def get_product_id(product_id: int):
-    product = get_product_by_id(product_id= product_id)
+def get_product_id(product_id: int, db: Session = Depends(get_db)):
+    product = get_product_by_id(db, product_id= product_id)
 
     if not product:
         raise HTTPException(status_code = 404, detail="Product not found!!")
@@ -26,23 +28,23 @@ def system_health():
     return {"message": "System is working fine"}
 
 @router.post("/")
-def add_product(product_details : Product):
-    product = create_product(product_details=product_details)
+def add_product(product_details : Product, db: Session = Depends(get_db)):
+    product = create_product(db, product_details=product_details)
     return product
 
 @router.put("/{product_id}")
-def modify_product(product_details : Product, product_id : int):
-    product = update_product(product_details=product_details, product_id=product_id)
+def modify_product(product_details : Product, product_id : int, db: Session = Depends(get_db)):
+    product = update_product(db, product_details=product_details, product_id=product_id)
     return product
 
 @router.delete("/{product_id}")
-def remove_product(product_id : int):
-    product = delete_product(product_id=product_id)
+def remove_product(product_id : int, db: Session = Depends(get_db)):
+    product = delete_product(db, product_id=product_id)
     return product
 
 @router.patch("/{product_id}")
-def update_partial_product(product_id: int, patch_details: Product):
-    result = patch_update(product_id=product_id, patch_update=patch_details)
+def update_partial_product(product_id: int, patch_details: ProductPatch, db: Session = Depends(get_db)):
+    result = patch_update(db, product_id=product_id, patch_update=patch_details)
 
     if result is None:
         raise HTTPException(status_code=404, detail="Product not found!!")
--- FastApi/ecommerce/app/services/product_service.py
+++ FastApi/ecommerce/app/services/product_service.py
@@ -1,80 +1,68 @@
-
-products = [
-    {
-        "id": 1,
-        "name": "Laptop",
-        "price": 50000,
-        "category": "Electronics",
-        "stock": 25
-    },
-    {
-        "id": 2,
-        "name": "Mobile",
-        "price": 25000,
-        "category": "Electronics",
-        "stock": 100
-    }
-]
+from sqlalchemy.orm import Session
+from app.db.base import ProductTable
 
 # GET All Products
-def get_all_products():
-    return products
+def get_all_products(db : Session):
+    return db.query(ProductTable).all()
 
 
 # GET Products by id
-def get_product_by_id(product_id: int):
-    for product in products:
-        if product["id"] == product_id:
-            return product
-    
-    return None
+def get_product_by_id(db: Session, product_id: int):
+    return db.query(ProductTable).filter(ProductTable.id == product_id).first()
 
 # POST to create product
-def create_product(product_details):
-    product = product_details.dict()
-    product["id"] = len(products) + 1
-    products.append(product)
-    return product
+def create_product(db: Session, product_details):
+    new_product = ProductTable(**product_details.dict())
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return new_product
 
 # PUT to update product
-def update_product(product_details, product_id):
-    new_product = product_details.dict()
-    new_product["id"] = product_id
-    updated = False
-    for i in range(0, len(products)):
-        if products[i]["id"] == product_id:
-            products[i] = new_product
-            updated = True
-            break
+def update_product(db: Session, product_details, product_id):
+    product = db.query(ProductTable).filter(ProductTable.id == product_id).first()
+
+    if not product:
+        return None
     
-    if updated:
-        return {"Updated Successfully"}
-    else:
-        return {"Product not found"}
+    for key, value in product_details.dict().items():
+        setattr(product, key, value)
+    
+    db.commit()
+    db.refresh(product)
+
+    return product
     
 
 # DELETE Product
-def delete_product(product_id):
-    index = None
-    for i in range(0, len(products)):
-        if products[i]["id"] == product_id:
-            index = i
-            break
+def delete_product(db: Session, product_id):
+    product = db.query(ProductTable).filter(ProductTable.id == product_id).first()
     
-    if index == None:
-        return {"Please enter a valid index"}
-    else:
-        products.pop(index)
-        return {"Product deleted successfully"}
+    if not product:
+        return None
+    
+    db.delete(product)
+    db.commit()
+
+    return product
+
+
 
 # PATCH Product
-def patch_update(product_id: int, patch_update):
+def patch_update(db: Session, product_id: int, patch_update):
+    product = db.query(ProductTable).filter(ProductTable.id == product_id).first()
     
-    for product in products:
-        if product["id"] == product_id:
-            patched_data = patch_update.dict(exclude_unset = True)
-            for key, value in patched_data.items():
-                product[key] = value
-            
-            return product
-    return None
+    if not product:
+        return None
+    
+    update_data = patch_update.dict(exclude_unset = True)
+
+    for key, value in update_data.items():
+        setattr(product, key, value)
+    
+    db.commit()
+    db.refresh(product)
+
+    return product

226579448fb68549e9d42499f08204db121a8049
madhur6712 <madhur.suresh@sigmoidanalytics.com>
2026-03-24 11:57:06 +0530

    Implemented Validation

--- participants/madhur-rao/week2/week2-day1/fast_api/ecommerce/app/models/product_model.py
+++ participants/madhur-rao/week2/week2-day1/fast_api/ecommerce/app/models/product_model.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , Field
 from typing import Optional
 
+# class Product(BaseModel):
+#     name:Optional[str] = None
+#     price:Optional[int] = None
+#     category:str = None
+#     stock:int = None
+
 class Product(BaseModel):
-    name:Optional[str] = None
-    price:Optional[int] = None
-    category:str = None
-    stock:int = None
+    name : str = Field(min_length=3,max_length=50)
+    price : int = Field(gt=0,lt=99999)
+    category : str = None
+    stock : int = None

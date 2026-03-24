b5e19fdba0bfe46a02bfbb7d1999ec36a16bc575
Abhi-Sigmoid-1309 <abhi.agarwal@sigmoidanalytics.com>
2026-03-24 18:46:06 +0530

    Created all CRUD operations based FastAPI backend project having async request handling along with Pydantic based data validation with multiple endpoints

--- week2-fastApi/app/db/database.py
+++ week2-fastApi/app/db/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Abhi%4013092003@localhost:3306/ecommerce_db"
+DATABASE_URL = "mysql+aiomysql://root:Abhi%4013092003@localhost:3306/ecommerce_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL,echo=True)
 
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 ) 
 
 Base = declarative_base()
--- week2-fastApi/main.py
+++ week2-fastApi/main.py
@@ -2,10 +2,26 @@ from fastapi import FastAPI
 from app.controllers.productController import router as productRouter
 from app.db.database import engine
 from app.db.base import Base
-app=FastAPI()
-# Create tables
-Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Create tables (ASYNC)
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# Run on startup
+@app.on_event("startup")
+async def on_startup():
+    await create_tables()
+
+
+# Include router
 app.include_router(productRouter)
+
+
 @app.get("/")
 def home():
-    return {"message":"FastApi server is running by abhi"}
+    return {"message": "FastApi server is running by abhi"}
